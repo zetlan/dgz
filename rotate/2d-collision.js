c62b215925d76520c5a98c514e0e4bc1f53e56e0
Davis Zetlan <davis@lewis.local>
2020-04-04 14:03:54 -0400

    Created file for organization

---
+++ rotate/2d-collision.js
@@ -0,0 +1,21 @@
+//functions for determining 2d collision are stored here, such as if two line segments intersect, if a point is inside a polygon, and other helpful functions relating to 2d position.
+
+
+function lineIntersect(lin1p1, lin1p2, lin2p1, lin2p2) {
+	//I chose i instead of l because l looks too similar to 1
+
+}
+
+function inPoly(xyPoint, polyPoints) {
+
+}
+
+function getOrientation(p1, p2, p3) {
+    //will return 0 if points are colinear, 1 if points are counterclockwise, and 2 if points are clockwise.
+    
+    //get slope between p1 and p2
+
+    //get slope between p2 and p3
+
+    //if 
+}
