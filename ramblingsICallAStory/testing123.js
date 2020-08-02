c9491ca260e5c74caf40130e1185864bb6ce0903
Davis Zetlan <davis@lewis.local>
2020-08-02 15:18:01 -0400

    Added chapters

--- ramblingsICallAStory/testing123.js
+++ ramblingsICallAStory/testing123.js
@@ -1,7 +1,7 @@
 var chapters_19843 = "'you fool. You egg. You absolute nutmeg.' As it turns out, two out of the three of those is a correct description of me. Well, I guess *was* a correct description of me. Now I am simply a fool.";
 var chapters_19844 = "This is a test chamber. This is a test. It has 4 walls and 87 floors.";
 
-var chapters_0 = ""
+var chapters_0 = ``;
 
 
 
@@ -15,6 +15,44 @@ Why must we separate our gates into binaries? Why must a NOT gate return a 1 whe
 <br>
 However. <br>
 <br>
-It has been nine hours and forty-five minutes since the last time I spoke to a NOT gate. They were a nice fellow, although technically, if you look closely, they are more complicated than just outputting a 1 when given a 0. The life of the NOT gate is a complex process, fully open to the whims of the electrons whizzing past on an ionizing wind. However, if you get to know them better, they are actually quite a nice fellow, and I am glad I could learn from them.<br>
+It has been nine hours and forty-five minutes since the last time I spoke to a NOT gate. They were a nice fellow, although technically, if you look closely, they are more complicated than just outputting a 1 when given a 0. The life of the NOT gate is a complex process, fully open to the whims of the electrons whizzing past on an ionizing wind. If you get to know them better, their lives become much more complex, and I am glad I could learn from them.<br>
 <br>
 I must go. The cappuccino calls.`;
+
+
+
+
+
+var chapters_3 = `<img src="image/pathUnknown123abc123abc123abc.png" alt="ERROR:images/pathUnknown/123abc123abc123abc.png/file_not_found"> The table wasteland is a most interesting location on the planet. Nobody really remembers how it came to be, but it is certainly a place that exists now, and few can live there. Legend has it that somewhere near the center of the table wasteland, an old carpenter sits, creating tables. However, others say that this is nonsense. <br>
+<br>
+The table wasteland is 47 miles in radius, and is located at approximately 45° above the equator. Why. The concept of assigning points to different geometries has always eluded me. On a euclidean plane, it is easy. One can assign each point to 2 numbers, one for the x axis, and one for the y axis. One can then use this system to assign coordinates, and they work when added, subtracted, multiplied, or otherwise operated on in most ways. <br>
+<br>
+On a spherical geometry, the process is similar. For when attempting to map the location of the table wasteland, it is easy to assign those 2 numbers not to points, but instead to angles, spun around perpendicular axes. Only 2 angles can describe any point on the surface of a sphere, and the concept is relatively easy to imagine in the world we live in. After all, humanity live on the surface of a sphere, so most of them are used to the concept.<br>
+<br>
+However, how can one assign a coordinate system to a hyperbolic plane? Lines curve. Trying to map the surface with squares and then projecting it down to a surface that is easily understandable fails when the coordinate system is ill-defined. I want to assign parallels, but there are too many parallel lines to choose just one. Traveling 180 degrees only sometimes lands a body where it started, and straight lines become arcs of a great circle, perpendicular to infinity. A hyperbolic world is a terrifying one, and this is the where the table wasteland lies. Too many tables are squished onto this tiny speck. More and more tables appear closer to the edges, until the great mass piles up, extruding into dimensions previously unknown. The carpenter has nary the time to fill the space around him<br>
+<br>
+<br>
+the table wasteland has a climate mostly defined by its ever present clouds and blustering wind. The wind always carries the clouds away from the area before they rain, meaning that although the average temperature is not particularly hot, the air is also extremely dry. Fires are occasionally an issue. The last fire was 273 years ago, and burned through 73% of the wasteland before it extinguished itself. It is unknown how the carpenter has maintained their own health through the centuries.<br> `;
+
+
+
+
+
+
+var chapters_4 = `Cynthia walks over to me from the chair she is sitting in. "Well, what is there left to say?" She queries. I sigh, unsure of how to articulate my thoughts.<br>
+<br>
+"I mean yes, I suppose we did work things out between ourselves, but there's still constant tension," I think of how Josh always gets in the way of a productive discussion, how Rob is unhelpful. Us two are really the only ones trying to push forwards, and that's kind of scary. "What if we don't survive?" I ask back. There's the constant threat of extinction, from space or from the many worlds around us.<br>
+<br>
+Bobby is stubborn. That's one of his many character flaws. "Well, then that's too bad for us I guess. But it's no reason not to try." He scowls at me. I don't understand why he can't just accept us and pilot the gosh darn ship. It's always like this. I know he's not trying to hold us back, but every day counts in the constantly changing universe. "That's scary though, this is our entire universe we're talking about. We can't just accept a universe without us in it."<br>
+<br>
+I think about the proposition. "Yes we can. And anyways, it's not like the universe can really just snuff us out as easily as that."<br>
+<br>
+I am confused. Cynthia's saying a lot of things that don't make sense. Of course the universe can just snuff us out, like the lives of so many of our compatriots. I voice these concerns, and she laughs. "None of those were really the universe's fault. It was all internal conflicts that eventually lead to their... departure." I have an easy counter-argument for that. "Well, won't going through with this plan involve internal conflicts?"<br>
+<br>
+"Yes, but that's a different type of internal conflict, the type where we learn and grow and become better people." <br>
+<br>
+We are both unhappy at this discussion.<br>
+<br>
+I stare out the window at the purple planet below, the occasional patch of orange catching my eye as bits of debris skate across the upper atmosphere. We have been orbiting the planet for quite a long time. It's only been a few weeks, but it feels like forever, since we keep having the same conversations over and over again. Are there other things to talk about? Of course. But for some reason, we keep coming back to this topic. <br>
+<br>
+"Drink some water, you two," Rob cuts in, I take a sip of water and`;
