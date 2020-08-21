379e2b5ef7a4ef9562c79df1f2a7210602267495
Davis Zetlan <davis@lewis.local>
2020-08-21 16:32:03 -0400

    Misc. changes for development

--- seasonalGrid/textures.js
+++ seasonalGrid/textures.js
@@ -2,29 +2,32 @@
 
 //shadow blocks are solid colors, so the function is simpler
 function drawMapShadow(ex, why, tileType) {
-    ctx.globalAlpha = 0.5
+	ctx.globalAlpha = 0.5;
 	//if it's a number, it'll be an exit block
 	var exitBlock = String(tileType).match(/^\d/);
-	if (exitBlock) {
-		
-	} else if (tileType != " ") {
-		//all regular tiles
-		var drawColor = color_error;
-		switch(tileType) {
-			case "A":
-				drawColor = color_wall;
-				break;
-			case "a":
-				drawColor = color_floor;
-				break;
-			default:
-				break;
-		}
+	var drawColor;
 
+	if (tileType != " ") {
+		if (exitBlock) {
+			drawColor = color_exit;
+		} else {
+			//all regular tiles
+			switch(tileType) {
+				case "A":
+					drawColor = color_wall;
+					break;
+				case "a":
+					drawColor = color_floor;
+					break;
+				default:
+					drawColor = color_error;
+					break;
+			}
+		}
 		ctx.fillStyle = drawColor;
-		drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
-    }
-    ctx.globalAlpha = 1;
+		drawPoly(ex, why, Math.ceil(tile_half / 0.89), 6, Math.PI / 6);
+	}
+	ctx.globalAlpha = 1;
 }
 
 //real map squares have textures, so the drawing is a bit more complex
@@ -32,21 +35,30 @@ function drawMapSquare(ex, why, tileType) {
 	//if it's a number, it'll be an exit block
 	var exitBlock = String(tileType).match(/^\d/);
 	if (exitBlock) {
+		//block
+		ctx.fillStyle = color_exit;
+		drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
+		ctx.fillStyle = color_exit_center;
+		drawPoly(ex, why, Math.ceil((tile_half * 0.6) / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
+
+		//number
+		ctx.fillStyle = color_text;
+		ctx.fillText(tileType, ex, why + tile_size * 0.25);
 		
 	} else if (tileType != " ") {
 		//all regular tiles
 		switch(tileType) {
 			case "A":
-                ctx.fillStyle = color_wall;
-                drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
+				ctx.fillStyle = color_wall;
+				drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
 				break;
 			case "a":
-                ctx.fillStyle = color_floor;
-                drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
+				ctx.fillStyle = color_floor;
+				drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
 				break;
 			default:
-                ctx.fillStyle = color_error;
-                drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
+				ctx.fillStyle = color_error;
+				drawPoly(ex, why, Math.ceil(tile_half / Math.sin(Math.PI / 3)), 6, Math.PI / 6);
 				break;
 		}
 		
