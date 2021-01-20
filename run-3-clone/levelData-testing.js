ccc85eddf49985fc81738662fcfd0982c52d657a
BobbyClementine <davis@zetlan.net>
2021-01-20 17:14:04 -0500

    Initial commit

---
+++ run-3-clone/levelData-testing.js
@@ -0,0 +1,6 @@
+var levelData_testing = `
+id-36|layout-tunnel20,1|color-0x000088|spawn-17|fallPropagate-0.04|terrain-pos-oomOooD005OooD005OooD00=OolD0@mOe/D5CmME0EEGmED0EEomAA1GcGD7EEm5E1EEOEEDEeEE5EEMEEAGDGD5l5o1oOoOd|terrain-pos-002P00[oojP00[oojP00[oobP03[o_2P:?[j/2RZoZZX2Z[oZZ02^^nX<X[hZZ2jZnZZPZZ[Z:ZZjZZRZZ^X[X[j3j0n0P0P8~crumbling
+id-18|layout-tunnel4,3|color-0x66FFFF|spawn-8|tileWidth-120|terrain-pos-Wo==MYXYW^fhfUX_7W_nh8Ok7n0
+
+
+`;
