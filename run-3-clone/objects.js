a8b20ff75881c324c96a8a8d7554cc0deaa808d5
BobbyClementine <davis@zetlan.net>
2021-01-25 09:55:55 -0500

    Camera behavior changes and character speed increase

--- run-3-clone/objects.js
+++ run-3-clone/objects.js
@@ -12,6 +12,10 @@ class Camera {
 		this.y = y;
 		this.z = z;
 
+		this.targetX = x;
+		this.targetY = y;
+		this.targetZ = z;
+
 		this.dx = 0;
 		this.dy = 0;
 		this.dz = 0;
@@ -26,60 +30,68 @@ class Camera {
 		this.phi = xRot;
 		this.rot = 0;
 		this.targetRot = 0;
+		this.animSteps = 9;
 
 		this.dt = 0;
 		this.dp = 0;
 	}
 
 	tick() {
-		//handling velocity
+		if (editor_active) {
+			//handling velocity
 
-		//adding
-		this.dx += this.ax;
+			//adding
+			this.dx += this.ax;
 
-		//binding max
-		if (Math.abs(this.dx) > this.dMax) {
-			this.dx *= 0.95;
-		}
+			//binding max
+			if (Math.abs(this.dx) > this.dMax) {
+				this.dx *= 0.95;
+			}
 
-		//friction
-		if (this.ax == 0) {
-			this.dx *= this.friction;
-		}
+			//friction
+			if (this.ax == 0) {
+				this.dx *= this.friction;
+			}
 
-		this.dz += this.az;
-		if (Math.abs(this.dz) > this.dMax) {
-			this.dz *= 0.95;
-		}
-		if (this.az == 0) {
-			this.dz *= this.friction;
-		}
-		
-		if (Math.abs(this.dy) > this.dMax) {
-			this.dy *= 0.95;
-		}
-
-		//handling position
-		var moveCoords = [0, 0, 0];
-		if (Math.abs(this.dz) > 0.05) {
-			var toAdd = polToCart(this.theta, this.phi, this.speed * 500 * this.dz);
-			moveCoords = [moveCoords[0] + toAdd[0], moveCoords[1] + toAdd[1], moveCoords[2] + toAdd[2]];
+			this.dz += this.az;
+			if (Math.abs(this.dz) > this.dMax) {
+				this.dz *= 0.95;
+			}
+			if (this.az == 0) {
+				this.dz *= this.friction;
+			}
 			
+			if (Math.abs(this.dy) > this.dMax) {
+				this.dy *= 0.95;
+			}
+
+			//handling position
+			var moveCoords = [0, 0, 0];
+			if (Math.abs(this.dz) > 0.05) {
+				var toAdd = polToCart(this.theta, this.phi, this.speed * 500 * this.dz);
+				moveCoords = [moveCoords[0] + toAdd[0], moveCoords[1] + toAdd[1], moveCoords[2] + toAdd[2]];
+				
+			}
+			if (Math.abs(this.dx) > 0.05) {
+				var toAdd = polToCart(this.theta + (Math.PI / 2), 0, this.speed * 500 * this.dx);
+				moveCoords = [moveCoords[0] + toAdd[0], moveCoords[1] + toAdd[1], moveCoords[2] + toAdd[2]];
+			}
+			this.x += moveCoords[0];
+			this.y += moveCoords[1];
+			this.z += moveCoords[2];
+		} else {
+			//changing with average
+			this.x = (this.targetX + (this.x * (this.animSteps - 1))) / this.animSteps;
+			this.y = (this.targetY + (this.y * (this.animSteps - 1))) / this.animSteps;
+			this.z = (this.targetZ + (this.z * (this.animSteps - 1))) / this.animSteps;
 		}
-		if (Math.abs(this.dx) > 0.05) {
-			var toAdd = polToCart(this.theta + (Math.PI / 2), 0, this.speed * 500 * this.dx);
-			moveCoords = [moveCoords[0] + toAdd[0], moveCoords[1] + toAdd[1], moveCoords[2] + toAdd[2]];
-		}
-		this.x += moveCoords[0];
-		this.y += moveCoords[1];
-		this.z += moveCoords[2];
 
 
 		//camera velocity
 		this.theta += this.dt;
 		this.phi += this.dp;
 		//weighted average towards target rotation
-		this.rot = (this.targetRot + (this.rot * 8)) / 9;
+		this.rot = (this.targetRot + (this.rot * (this.animSteps - 1))) / this.animSteps;
 
 		//special case for vertical camera orientation
 		if (Math.abs(this.phi) >= Math.PI * 0.5) {
@@ -95,13 +107,15 @@ class Character {
 		this.dir_down = [0, Math.PI / 2];
 		this.dir_side = [0, 0];
 		this.dir_front = [Math.PI / 2, 0];
-		this.gravStrength = 0.04;
-		this.speed = 0.1;
-		this.dMax = 1.2;
+		this.gravStrength = 0.08;
+		this.speed = 0.15;
+		this.dMax = 2;
 
 		this.x = x;
 		this.y = y;
 		this.z = z;
+
+		this.refPoint = [this.x, this.y + 10, this.z];
 
 		this.dx = 0;
 		this.dy = 0;
@@ -111,7 +125,7 @@ class Character {
 		this.az = 0;
 		this.friction = 0.85;
 
-		this.r = 1000;
+		this.r = 10;
 		this.cameraDist = 1000;
 		this.drawR = this.r / getDistance(this, world_camera);
 		this.color = "#888";
@@ -119,9 +133,18 @@ class Character {
 	}
 
 	tick() {
+		//setting camera position
+		var vertOffset = polToCart(this.dir_down[0], this.dir_down[1], 100);
+		var horizOffset = polToCart(this.dir_front[0], this.dir_front[1], -100);
+		world_camera.targetX = this.x + vertOffset[0] + horizOffset[0];
+		world_camera.targetY = this.y + vertOffset[1] + horizOffset[1];
+		world_camera.targetZ = this.z + vertOffset[2] + horizOffset[2];
+
+
+
 		//getting camera distance
 		this.cameraDist = Math.max(1, getDistance(this, world_camera) - 20);
-		this.drawR = this.r / this.cameraDist;
+		//this.drawR = this.r / this.cameraDist;
 
 		//modifying forces
 		this.dy -= this.gravStrength;
@@ -141,8 +164,8 @@ class Character {
 		if (this.az == 0) {
 			this.dz *= this.friction;
 		}
-		if (Math.abs(this.dz) > this.dMax) {
-			this.dz = clamp(this.dz, -1 * this.dMax, this.dMax);
+		if (Math.abs(this.dz) > this.dMax * 2) {
+			this.dz = clamp(this.dz, -2 * this.dMax, 2 * this.dMax);
 		}
 
 		//moving according to forces
@@ -153,12 +176,16 @@ class Character {
 		this.x += gravForce[0] + turnForce[0] + frontForce[0];
 		this.y += gravForce[1] + turnForce[1] + frontForce[1];
 		this.z += gravForce[2] + turnForce[2] + frontForce[2];
+		this.refPoint = [this.x, this.y + this.r, this.z];
 	}
 
 	beDrawn() {
 		if (!isClipped([this.x, this.y, this.z])) {
 			var [tX, tY] = spaceToScreen([this.x, this.y, this.z]);
-			drawCircle(this.color, tX, tY, this.drawR);
+			var [tX2, tY2] = spaceToScreen(this.refPoint);
+			var r = Math.sqrt(((tX - tX2) * (tX - tX2)) + ((tY -tY2) * (tY -tY2)));
+			drawCircle("#000", tX, tY, r);
+			drawCircle(this.color, tX, tY, r - 4);
 			
 			if (editor_active) {
 				var cartX = polToCart(this.dir_side[0], this.dir_side[1], 10);
@@ -184,82 +211,6 @@ class Character {
 				ctx.moveTo(tX, tY);
 				ctx.lineTo(zXY[0], zXY[1]);
 				ctx.stroke();
-			}
-		}
-	}
-
-
-
-}
-
-
-class TreeNode {
-	constructor(contains) {
-		this.contains = contains;
-		this.inObj = undefined;
-		this.outObj = undefined;
-	}
-
-	//passes object to a spot below the self
-	accept(object) {
-		var ref = this.contains;
-		var outputs = object.clipAtPlane([ref.x, ref.y, ref.z], [ref.normal[0], ref.normal[1]]);
-
-		//if the object in the below bucket is not defined, push output to below bucket
-		if (outputs[0] != undefined) {
-			if (this.inObj == undefined) {
-				this.inObj = new TreeNode(outputs[0]);
-			} else {
-				//if there is something in the below bucket, make sure that the output is valid before making it the below bucket's problem
-				this.inObj.accept(outputs[0]);
-			}
-		}
-
-		if (outputs[1] != undefined) {
-			if (this.outObj == undefined) {
-				this.outObj = new TreeNode(outputs[1]);
-			} else {
-				this.outObj.accept(outputs[1]);
-			}
-		}
-	}
-
-	traverse(tick) {
-		//getting the dot product of angles between self normal and camera normal
-		var v1 = polToCart(this.contains.normal[0], this.contains.normal[1], 1);
-		var v2 = [world_camera.x - this.contains.x, world_camera.y - this.contains.y, world_camera.z - this.contains.z];
-
-		var dot = (v1[0] * v2[0]) + (v1[1] * v2[1]) + (v1[2] * v2[2]);
-
-		//traverse in reverse order if the dot product is negative
-		if (dot > 0) {
-			//right
-			if (this.outObj != undefined) {
-				this.outObj.traverse(tick);
-			}
-
-			//center
-			if (tick) {
-				this.contains.tick();
-			} else {
-				this.contains.beDrawn();
-			}
-
-			//left
-			if (this.inObj != undefined) {
-				this.inObj.traverse(tick);
-			}
-		} else {
-			if (this.inObj != undefined) {
-				this.inObj.traverse(tick);
-			}
-			if (tick) {
-				this.contains.tick();
-			} else {
-				this.contains.beDrawn();
-			}
-			if (this.outObj != undefined) {
-				this.outObj.traverse(tick);
 			}
 		}
 	}
