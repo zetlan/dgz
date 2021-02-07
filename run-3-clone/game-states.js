32c387bcf94d88d688e47ab17a112425a86bbfaa
BobbyClementine <davis@zetlan.net>
2021-02-07 12:06:49 -0500

    Added infinite mode / menu

--- run-3-clone/game-states.js
+++ run-3-clone/game-states.js
@@ -1,3 +1,46 @@
+
+class State_Cutscene {
+	constructor() {
+		this.frame = 0;
+	}
+
+	execute() {
+		//bege / stars
+		ctx.fillStyle = color_bg;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+		world_stars.forEach(s => {
+			s.beDrawn();
+		});
+
+		world_camera.tick();
+		
+		player.x = world_camera.x;
+		player.y = world_camera.y;
+		player.z = world_camera.z;
+
+		//order objects every once in a while
+		if (world_time % 40 == 3) {
+			world_objects.forEach(w => {
+				w.tick();
+			});
+			//order all objects
+			world_objects = orderObjects(world_objects, 7);
+		}
+
+		//draw all tunnels
+		world_objects.forEach(w => {
+			w.beDrawn();
+		});
+	}
+}
+
+
+
+
+
+
+
 class State_Game {
 	constructor() {
 		this.text = ``;
@@ -10,7 +53,7 @@ class State_Game {
 	}
 
 	execute() {
-		logTime("total");
+		//logTime("total");
 		//handling entities
 		world_camera.tick();
 		player.tick();
@@ -73,9 +116,9 @@ class State_Game {
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 		
 		//draw stars
-		for (var c=0;c<world_stars.length;c++) {
-			world_stars[c].beDrawn();
-		}
+		world_stars.forEach(c => {
+			c.beDrawn();
+		});
 
 		//drawing all tunnels
 		this.farObjs.forEach(f => {
@@ -85,47 +128,12 @@ class State_Game {
 			if (f != player.parent) {
 				f.beDrawn();
 			}
-		})
+		});
 
 		if (player.parent == undefined) {
 			player.beDrawn();
 		} else {
-			//sorting player in with the parent tunnel to be drawn
-			var stripStorage = orderObjects(player.parent.strips, 4);
-			
-			//if the player is in the middle of the strips (on top of some but not all) do the special
-			if (stripStorage[0].playerIsOnTop() != stripStorage[stripStorage.length-1].playerIsOnTop()) {
-				var drawPlayer = true;
-				stripStorage.forEach(t => {
-					if (drawPlayer && t.playerIsOnTop()) {
-						t.beDrawn();
-					} else if (drawPlayer) {
-						drawPlayer = false;
-						player.beDrawn();
-						t.beDrawn();
-					} else {
-						t.beDrawn();
-					}
-				});
-				if (drawPlayer) {
-					player.beDrawn();
-				}
-			} else {
-				//case where player is below all
-				if (!stripStorage[0].playerIsOnTop()) {
-					player.beDrawn();
-					stripStorage.forEach(t => {
-						t.beDrawn();
-					});
-				} else {
-					//case where player is above all
-					stripStorage.forEach(t => {
-						t.beDrawn();
-					});
-					player.beDrawn();
-				}
-			} 
-			
+			drawPlayerWithParent();
 		}
 
 		//crosshair
@@ -144,7 +152,7 @@ class State_Game {
 			ctx.fillText(this.text, canvas.width * 0.5, canvas.height * 0.5);
 			this.time -= 1;
 		}
-		logTimeEnd("total", "avg. frame time");
+		//logTimeEnd("total", "avg. frame time");
 	}
 
 	orderWorld() {
@@ -166,11 +174,220 @@ class State_Game {
 
 		this.nearObjs = orderObjects(this.nearObjs, 5);
 	}
+
+	handleEscape() {
+		loading_state = new State_Map();
+	}
+
+	handleMouseDown(a) {
+
+	}
+
+	handleMouseMove(a) {
+
+	}
+}
+
+class State_Infinite {
+	constructor() {
+		this.time = 0;
+		this.meters = 0;
+		this.data = levelData_infinite.split("\n");
+		this.difficulty = 0;
+
+		this.objs = [];
+		this.sortedObjs = [];
+		for (var a=0; a<5; a++) {
+			this.addTunnel();
+		}
+		this.placePlayer();
+		ctx.lineWidth = 2;
+	}
+
+	addTunnel() {
+		//if a tunnel is already there, set start coords to that tunnel end
+		var [sX, sZ, sT] = [0, 0, 0, 0];
+		if (this.objs.length > 0) {
+			sT = this.objs[this.objs.length - 1].theta;
+			sX = this.objs[this.objs.length - 1].endPos[0] + (tunnel_transitionLength * Math.sin(sT));
+			sZ = this.objs[this.objs.length - 1].endPos[2] + (tunnel_transitionLength * Math.cos(sT));
+			
+		}
+
+		//randomly change theta a bit, the complication is to stay within bounds (0 - 2pi)
+		sT = (sT + (Math.random() * 0.6) + ((Math.PI * 2) - 0.3)) % (Math.PI * 2);
+
+		this.objs.push(new Tunnel_FromData(this.data[Math.floor(randomBounded(this.difficulty, this.difficulty + infinite_levelRange))]));
+		this.objs[this.objs.length-1].theta = sT;
+		this.objs[this.objs.length-1].updatePosition([sX, 0, sZ]);
+		
+		//updating difficulty
+		this.difficulty += 1;
+		if (this.difficulty + infinite_levelRange > this.data.length-1) {
+			this.difficulty -= 1;
+		}
+		this.orderWorld();
+	}
+
+	placePlayer() {
+		//putting player into world officially
+		player.parentPrev = this.objs[0];
+		player.parent = player.parentPrev;
+		player.parentPrev.reset();
+		
+		loading_state.text = player.parentPrev.id;
+		loading_state.time = render_tunnelTextTime;
+	}
+
+	execute() {
+		//logTime("total");
+
+		//add to meter count, 30 pixels in a meter
+		this.meters += player.dz / 30;
+
+		//handling entities
+		world_camera.tick();
+		player.tick();
+
+		//if the player isn't in a tunnel, try to get them in one
+		if (player.parent == undefined) {
+			this.orderWorld();
+
+			//try the player in the closest few tunnels
+			for (var v=this.sortedObjs.length-1; v>=0; v--) {
+				if (this.sortedObjs[v].playerIsInBounds()) {
+					player.parent = this.sortedObjs[v];
+					v = -1;
+				}
+			}
+			//if they're still undefined, kill them
+			if (player.parent == undefined) {
+				if (this.meters > 20) {
+					loading_state = new State_Menu();
+				} else {
+					player.parentPrev.reset();
+					player.parent = player.parentPrev;
+					this.meters = 0;
+				}
+				
+			} else {
+				if (player.parent != player.parentPrev) {
+					//display text
+					this.text = player.parent.id;
+					this.time = render_tunnelTextTime;
+
+					//remove first level and add new level
+					this.objs.splice(0, 1);
+					this.addTunnel();
+				}
+			}
+		} else { 
+			//if the player's left their parent tunnel, change their parent
+			if (!player.parent.playerIsInBounds()) {
+				player.parentPrev = player.parent;
+				player.parent = undefined;
+			} else if (!player.parent.playerIsInTunnel()) {
+				
+				//if the player is in the void, try to change parent without reordering objects
+				for (var v=this.sortedObjs.length-1; v>=0; v--) {
+					if (this.sortedObjs[v].playerIsInTunnel()) {
+						player.parentPrev = player.parent;
+						player.parent = this.sortedObjs[v];
+						v = -1;
+
+						//reorder objects anyways if found a new tunnel
+						this.orderWorld();
+
+						//text / level stuffies
+						this.text = player.parent.id;
+						this.time = render_tunnelTextTime;
+						this.objs.splice(0, 1);
+						this.addTunnel();
+					}
+				}
+			}
+		}
+
+		
+		//tick tunnels
+		this.sortedObjs.forEach(v => {
+			v.tick();
+		});
+		
+
+		//drawing background
+		ctx.fillStyle = color_bg;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		
+		//draw stars
+		world_stars.forEach(c => {
+			c.beDrawn();
+		});
+
+		//drawing all tunnels
+		this.sortedObjs.forEach(f => {
+			if (f != player.parent) {
+				f.beDrawn();
+			}
+		});
+
+		if (player.parent == undefined) {
+			player.beDrawn();
+		} else {
+			drawPlayerWithParent();
+		}
+
+		//crosshair
+		if (editor_active) {
+			drawCrosshair();
+		}
+
+
+		//drawing pressed keys
+		drawKeys();
+
+		//drawing meter text
+		ctx.fillStyle = color_text_bright;
+		ctx.font = `${canvas.height / 22}px Century Gothic`;
+		ctx.fillText(this.meters.toFixed(2), canvas.width * 0.1, canvas.height * 0.05);
+
+		//drawing new tunnel text
+		if (this.time > 0) {
+			ctx.font = `${canvas.height / 22}px Century Gothic`;
+			ctx.fillText(this.text, canvas.width * 0.5, canvas.height * 0.5);
+			this.time -= 1;
+		}
+		
+		//logTimeEnd("total", "avg. frame time");
+	}
+
+	orderWorld() {
+		//ordering all the objects
+		this.objs.forEach(u => {
+			u.getCameraDist();
+		});
+		this.sortedObjs = orderObjects(this.objs, 5);
+	}
+
+	handleEscape() {
+		loading_state = new State_Menu();
+	}
+
+	handleMouseDown(a) {
+		
+	}
+
+	handleMouseMove(a) {
+
+	}
 }
 
 class State_Loading {
 	constructor() {
 		this.time = 10;
+		this.levelSets = data_levelSets;
+
+		//this.levelSets = [`M`, `D`, `F`];
 	}
 
 	execute() {
@@ -187,63 +404,34 @@ class State_Loading {
 			drawCircle(color_stars, (canvas.width * 0.5) + xAdd, (canvas.height * 0.5) + yAdd, randomSeeded(3, 7));
 
 			//loading tunnels at certain times
-			//TODO: refactor this, it's difficult to read
-			switch (this.time) {
-				case 11:
-					placeTunnelSet(levelData_mainTunnel);
-					console.log(`placed main tunnel`);
-					break;
-				case 20:
-					placeTunnelSet(levelData_boxStorage);
-					console.log(`placed box storage`);
-					break; 
-				case 40:
-					placeTunnelSet(levelData_winterGames);
-					console.log(`placed winter games`);
-					break;
-				case 60:
-					placeTunnelSet(levelData_lowPower);
-					console.log(`placed low power`);
-					break;
-				case 80:
-					placeTunnelSet(levelData_A);
-					console.log(`placed A`);
-					break;
-				case 100:
-					placeTunnelSet(levelData_B);
-					console.log(`placed B`);
-					break;
-				case 120:
-					placeTunnelSet(levelData_D);
-					console.log(`placed D`);
-					break;
-				case 140:
-					placeTunnelSet(levelData_G);
-					console.log(`placed G`);
-					break;
-				case 160:
-					placeTunnelSet(levelData_L);
-					console.log(`placed L`);
-					break;
-				case 180:
-					placeTunnelSet(levelData_T);
-					console.log(`placed T`);
-					break;
-				case 200:
-					placeTunnelSet(levelData_U);
-					console.log(`placed U`);
-					break;
-				case 220:
-					placeTunnelSet(levelData_W);
-					console.log(`placed W`);
-					break;
-				default:
-					break;
+			if (this.time % 20 == 0) {
+				var index = (this.time / 20) - 1;
+				
+				if (index < this.levelSets.length) {
+					//loading in a level set
+					placeTunnelSet(eval(`levelData_${this.levelSets[index]}`));
+					console.log(`placed ${this.levelSets[index]}`);
+				}
+
+				if (this.time == 540) {
+					//story things after loading all levels
+					localStorage_read();
+					getObjectFromID(`Level 1`).discovered = true;
+					console.log(`loaded save`);
+				}
 			}
 		}
 		if (this.time > 550) {
-			loading_state = new State_Map();
+			loading_state = new State_Menu();
 		}
+	}
+
+	handleMouseDown(a) {
+
+	}
+
+	handleMouseMove(a) {
+
 	}
 }
 
@@ -338,7 +526,204 @@ class State_Map {
 			}
 			
 		}
+	}
 
+	handleEscape() {
+		loading_state = new State_Menu();
+	}
+
+
+	//mouse handling functions, ack this is ugly
+	handleMouseDown(a) {
+		if (editor_active) {
+			var canvasArea = canvas.getBoundingClientRect();
+			if (editor_selected != undefined) {
+				var knobCoords = [editor_selected.map_startCoords[0] + (editor_thetaCircleRadius * Math.cos(editor_selected.theta)), editor_selected.map_startCoords[1] - (editor_thetaCircleRadius * Math.sin(editor_selected.theta))];
+	
+				//if colliding with the theta change circle, do that stuff
+				if (getDistance2d(knobCoords, [Math.round(a.clientX - canvasArea.left), Math.round(a.clientY - canvasArea.top)]) < editor_thetaKnobRadius) {
+					editor_changingTheta = true;
+					var diffX = Math.round(a.clientX - canvasArea.left) - cursor_x;
+					var diffY = -1 * (Math.round(a.clientY - canvasArea.top) - cursor_y);
+					editor_selected.theta = (Math.atan2(diffY, diffX) + (Math.PI * 2)) % (Math.PI * 2);
+					editor_selected.updatePosition([editor_selected.x, editor_selected.y, editor_selected.z]);
+				} else {
+					editor_changingTheta = false;
+					editor_selected = undefined;
+				}
+			} else {
+				//update cursor position
+				cursor_x = Math.round(a.clientX - canvasArea.left);
+				cursor_y = Math.round(a.clientY - canvasArea.top);
+	
+				//modifying regular tunnels
+	
+				//loop through all world objects, if one is close enough, make it the selected one
+				for (var a=0; a<world_objects.length; a++) {
+					if (getDistance2d(world_objects[a].map_circleCoords, [cursor_x, cursor_y]) < editor_clickTolerance) {
+						editor_selected = world_objects[a];
+						cursor_x = world_objects[a].map_circleCoords[0];
+						cursor_y = world_objects[a].map_circleCoords[1];
+						a = world_objects.length + 1;
+					}
+				}
+			}
+		} else {
+			//going into the level that's selected
+			if (loading_state instanceof State_Map && loading_state.levelSelected != undefined) {
+				//ordering all the objects
+				world_objects.forEach(u => {
+					u.getCameraDist();
+				});
+				world_objects = orderObjects(world_objects, 8);
+				player.parentPrev = loading_state.levelSelected;
+				loading_state = new State_Game();
+				player.parentPrev.reset();
+	
+				//displaying text
+				loading_state.text = player.parentPrev.id;
+				loading_state.time = render_tunnelTextTime;
+			}
+		}
+	}
+
+	handleMouseMove(a) {
+		var canvasArea = canvas.getBoundingClientRect();
+
+		if (editor_active) {
+			if (cursor_down && editor_selected != undefined) {
+				if (editor_changingTheta) {
+					//update selected tunnel direction
+					var diffX = Math.round(a.clientX - canvasArea.left) - cursor_x;
+					var diffY = -1 * (Math.round(a.clientY - canvasArea.top) - cursor_y);
+					editor_selected.theta = (Math.atan2(diffY, diffX) + (Math.PI * 2)) % (Math.PI * 2);
+					editor_selected.updatePosition([editor_selected.x, editor_selected.y, editor_selected.z]);
+					cursor_x = editor_selected.map_circleCoords[0];
+					cursor_y = editor_selected.map_circleCoords[1];
+				} else {
+					//moving the tunnel
+					cursor_x = Math.round(a.clientX - canvasArea.left);
+					cursor_y = Math.round(a.clientY - canvasArea.top);
+
+					var snapX = cursor_x;
+					var snapY = cursor_y;
+
+					//if a tunnel end is close enough to the tunnel start, snap the tunnel to that position
+					var startSelectOffset = [editor_selected.map_startCoords[0] - editor_selected.map_circleCoords[0], editor_selected.map_startCoords[1] - editor_selected.map_circleCoords[1]];
+					//calculating tunnel end pos
+					for (var a=0; a<world_objects.length; a++) {
+						if (world_objects[a] != editor_selected) {
+							var endPos = world_objects[a].map_endCoords;
+							if (getDistance2d([endPos[0], endPos[1]], [snapX + startSelectOffset[0], snapY + startSelectOffset[1]]) < editor_snapTolerance) {
+								//moving position of selection
+								//get difference between tunnel start coordinates and selected coordinates
+								snapX = endPos[0] - startSelectOffset[0];
+								snapY = endPos[1] - startSelectOffset[1];
+							}
+						}
+					}
+
+					//update selected tunnel position
+					if (editor_selected != undefined) {
+						var offset = [editor_selected.map_startCoords[0] - editor_selected.map_circleCoords[0], editor_selected.map_startCoords[1] - editor_selected.map_circleCoords[1]];
+						var newCoords = screenToSpace([snapX + offset[0], snapY + offset[1]], map_cameraHeight);
+						editor_selected.updatePosition(newCoords);
+					}
+				}
+			}
+		} else {
+			if (loading_state instanceof State_Map) {
+				//if mousing over a level, set the ID to the display text
+				var testX = Math.round(a.clientX - canvasArea.left);
+				var testY = Math.round(a.clientY - canvasArea.top);
+
+				//resetting both text and position
+				loading_state.levelSelected = undefined;
+				loading_state.cursorPos = [-100, -100];
+
+				//if a tunnel end is close enough, snap the tunnel to that position
+				//calculating tunnel end pos
+				for (var a=0; a<world_objects.length; a++) {
+					var tunnelPos = world_objects[a].map_circleCoords;
+					if (getDistance2d([tunnelPos[0], tunnelPos[1]], [testX, testY]) < editor_snapTolerance && world_objects[a].discovered) {
+						loading_state.cursorPos = tunnelPos;
+						loading_state.levelSelected = world_objects[a];
+						a = world_objects.length + 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+class State_Menu {
+	constructor() {
+		ctx.strokeStyle = color_grey_dark;
+
+		this.overButton = -1;
+		this.buttons = menu_buttons;
+	}
+
+	execute() {
+		//bege
+		ctx.fillStyle = color_bg;
+		ctx.strokeStyle = color_grey_dark;
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		//title card
+		ctx.font = `${canvas.height / 10}px Century Gothic`;
+		ctx.textAlign = `center`;
+		ctx.fillStyle = color_text_bright;
+		ctx.fillText(`Run 3`, canvas.width * 0.5, canvas.height * 0.1);
+
+		//character circle
+		
+		//mode select
+
+		//drawing buttons
+		var trueButtonWidth = (canvas.width * menu_buttonWidth * 0.5);
+		var trueButtonHeight = (canvas.height * menu_buttonHeight * 0.5);
+
+		ctx.lineWidth = canvas.height / 96;
+		ctx.font = `${canvas.height / 30}px Century Gothic`;
+		for (var y=0; y<this.buttons.length; y++) {
+			var yOffset = ((this.buttons.length - 1) * trueButtonHeight * 2) * y;
+			if (this.overButton == y) {
+				ctx.fillStyle = color_grey_dark;
+			} else {
+				ctx.fillStyle = color_grey_light;
+			}
+			drawRoundedRectangle((canvas.width * 0.5) - trueButtonWidth, (canvas.height * 0.5) - (canvas.height * menu_buttonHeight * 0.5) + yOffset, trueButtonWidth * 2, trueButtonHeight * 2, canvas.height / 48);
+			
+			//text
+			ctx.fillStyle = color_text;
+			ctx.fillText(this.buttons[y][0], canvas.width * 0.5, (canvas.height * 0.5) + yOffset + (canvas.height / 90))
+		}
+	}
+
+	handleMouseDown(a) {
+		if (this.overButton > -1) {
+			loading_state = eval(this.buttons[this.overButton][1]);
+		}
+	}
+
+	handleMouseMove(a) {
+		var canvasArea = canvas.getBoundingClientRect();
+		cursor_x = a.clientX - canvasArea.left;
+		cursor_y = a.clientY - canvasArea.top;
+
+		this.overButton = -1;
+
+		//check for collision with center buttons
+		var trueButtonWidth = (canvas.width * menu_buttonWidth * 0.5);
+		var trueButtonHeight = (canvas.height * menu_buttonHeight * 0.5);
+		for (var y=0; y<this.buttons.length; y++) {
+			var yOffset = ((this.buttons.length - 1) * trueButtonHeight * 2) * y;
+
+			if (cursor_x > (canvas.width * 0.5) - trueButtonWidth && cursor_x < (canvas.width * 0.5) + trueButtonWidth &&
+				cursor_y > (canvas.height * 0.5) + yOffset - trueButtonHeight && cursor_y < (canvas.height * 0.5) + yOffset + trueButtonHeight) {
+					this.overButton = y;
+			}
+		}
 	}
 }
 
