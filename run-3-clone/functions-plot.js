b8701ec50f3bf81fc6aeb26043f5260f0acc389d
BobbyClementine <davis@zetlan.net>
2021-03-18 19:57:54 -0400

    Initial commit

---
+++ run-3-clone/functions-plot.js
@@ -0,0 +1,11 @@
+
+/* 
+INDEX:
+	unlockCharacter();
+*/
+
+
+
+function unlockCharacter(characterName) {
+	if (!data_persistent.unlocked.includes("Gentleman")) {data_persistent.unlocked.push("Gentleman");}
+}
