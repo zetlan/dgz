ee3049b0c275ee463720d051447dc46efb2ac961
BobbyClementine <davis@zetlan.net>
2021-03-18 20:00:21 -0400

    Minor fixes

--- run-3-clone/game-states-editor.js
+++ run-3-clone/game-states-editor.js
@@ -2,8 +2,8 @@ class State_Edit {
 	constructor() {
 		
 		player = new Runner(player.x, player.y, player.z);
-		this.tunnel = new Tunnel(0.1, {h: 0, s: 0}, [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], `Custom Tunnel 1`, 40, 1, [], 4, [], [], 4, 70, 0, 0);
-
+		this.tunnel = new Tunnel(0.1, {h: 0, s: 0, v: 0.5}, [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]], `Custom Tunnel 1`, 40, 1, [], 4, [], [], 4, 70, 0, 0);
+		
 		world_camera.targetX = 0.1;
 		world_camera.targetY = 5;
 		world_camera.targetZ = 0;
@@ -14,11 +14,14 @@ class State_Edit {
 		world_camera.rot = 0;
 		//so they can see plexiglass tiles
 		player = new Pastafarian(player.x, player.y, player.z);
+		player.parent = this.tunnel;
+		player.parentPrev = this.tunnel;
 		render_maxColorDistance *= editor_colorMultiplier;
 
 
 		this.frontHeld = false;
 		this.sideHeld = false;
+		this.topDarkButton = undefined;
 	}
 
 	execute() {
@@ -27,12 +30,12 @@ class State_Edit {
 
 		//little dot in the center of the screen
 		drawCircle(color_editor_cursor, canvas.width * 0.5, canvas.height * 0.5, 2);
-		
+
 		//top bar
 		ctx.fillStyle = color_editor_bg;
 		ctx.fillRect(0, 0, canvas.width, canvas.height * editor_topBarHeight);
 
-		//buttons
+		//top buttons
 		ctx.font = `${Math.floor(canvas.height * 0.04)}px Comfortaa`;
 		ctx.textAlign = `center`;
 		ctx.fillStyle = color_grey_light;
@@ -40,13 +43,13 @@ class State_Edit {
 		ctx.lineWidth = canvas.height / 96;
 		for (var a=1; a<=editor_buttons.length; a++) {
 			var width = canvas.width * 1 / (editor_buttons.length * 1.5);
-			if (loading_state.substate == a) {
+			if (this.topDarkButton == a) {
 				ctx.fillStyle = color_grey_dark;
 			}
 			drawRoundedRectangle((canvas.width * (a / (editor_buttons.length + 1))) - (width * 0.5), canvas.height * 0.03, width, canvas.height * 0.06, canvas.height / 40);
 
 			ctx.fillStyle = color_text;
-			ctx.fillText(editor_buttons[a-1], (canvas.width * (a / (editor_buttons.length + 1))), canvas.height * 0.075);
+			ctx.fillText(editor_buttons[a-1][0], (canvas.width * (a / (editor_buttons.length + 1))), canvas.height * 0.075);
 			ctx.fillStyle = color_grey_light;
 		}
 	}
@@ -57,7 +60,7 @@ class State_Edit {
 		player.y = world_camera.y;
 		player.z = world_camera.z;
 		player.onGround = 1;
-		player.dy *= 0.02;
+		player.dy = clamp(player.dy-(player.jumpStrength * 0.5), 0, 10);
 
 		//translate player up into camera movement
 		if (player.dy == 0) {
@@ -102,6 +105,10 @@ class State_Edit {
 	}
 
 	handleMouseDown(a) {
+		var canvasArea = canvas.getBoundingClientRect();
+		cursor_x = Math.round(a.clientX - canvasArea.left);
+		cursor_y = Math.round(a.clientY - canvasArea.top);
+
 		//changing state
 		if (cursor_y < canvas.height * 0.12) {
 			for (var a=0; a<editor_buttons.length; a++) {
@@ -110,12 +117,9 @@ class State_Edit {
 
 				if (cursor_x > (canvas.width * ((a+1) / (editor_buttons.length + 1))) - width && cursor_x < (canvas.width * ((a+1) / (editor_buttons.length + 1))) + width &&
 				cursor_y > (canvas.height * 0.06) - height && cursor_y < (canvas.height * 0.06) + height) {
-					if (loading_state.constructor.name == eval(editor_buttons[a][1]).constructor.name) {
-						loading_state = new State_Edit();
-						loading_state.tunnel = this.tunnel;
-					} else {
+					if (loading_state.constructor.name != eval(editor_buttons[a][1]).constructor.name) {
 						loading_state = eval(editor_buttons[a][1]);
-						loading_state.tunnel = this.tunnel;
+						this.transferPropertiesToState(loading_state);
 					}
 					return 31;
 				}
@@ -129,14 +133,19 @@ class State_Edit {
 		player = new Runner(player.x, player.y, player.z);
 		loading_state = new State_Menu();
 	}
+
+	transferPropertiesToState(newState) {
+		newState.tunnel = this.tunnel;
+		player.parent = this.tunnel;
+		player.parentPrev = this.tunnel;
+		render_maxColorDistance /= editor_colorMultiplier;
+	}
 }
 
 
 class State_Edit_Tiles extends State_Edit {
 	constructor() {
 		super();
-		player.parent = this.tunnel;
-		player.parentPrev = this.tunnel;
 
 		this.tileSelected = 0;
 		this.animTileSelected = 0;
@@ -146,6 +155,7 @@ class State_Edit_Tiles extends State_Edit {
 		this.selectedTileExtra = new Tile_Plexiglass(tileCoords[0], tileCoords[1], tileCoords[2], this.tunnel.tileSize, this.tunnel.strips[this.targetTile[0]].normal, this.tunnel, this.targetTile, this.tunnel.color, 0.5);
 		this.selectedTileExtra.playerDist = 50;
 		this.selectedTileExtra.cameraDist = 50;
+		this.topDarkButton = 1;
 	}
 
 	execute() {
@@ -255,6 +265,16 @@ class State_Edit_Tiles extends State_Edit {
 			return;
 		}
 
+		//if it's at the start, toggle a spawn rather than creating a tile
+		if (this.targetTile[1] == -1) {
+			var spawnArr = this.tunnel.spawns;
+			if (!spawnArr.includes(this.targetTile[0])) {
+				spawnArr.push(this.targetTile[0]);
+			} else {
+				spawnArr.splice(spawnArr.indexOf(this.targetTile[0]), 1);
+			}
+			return;
+		}
 		//place tile there if no other actions have been taken
 		this.modifySelectedTilePos();
 	}
@@ -280,38 +300,38 @@ class State_Edit_Properties extends State_Edit {
 
 		this.propertyModifiers = [
 			//ew, this is verbose
-			//hue + saturation
+			//hue + saturation + value
 			new PropertySlider(0.01, 0.14 + (0 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `hue`, `loading_state.tunnel.color.h = value;`, `loading_state.tunnel.color.h`, 0, 360, 1, false),
 			new PropertySlider(0.01, 0.14 + (1 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `sat.`, `loading_state.tunnel.color.s = value;`, `loading_state.tunnel.color.s`, 0, 80, 1, false),
+			new PropertySlider(0.01, 0.14 + (2 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `val.`, `loading_state.tunnel.color.v = value;`, `loading_state.tunnel.color.v`, 0, 0.9, 0.001, false),
 
 			//power
-			new PropertySlider(0.01, 0.14 + (3 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `power`, `loading_state.tunnel.powerBase = value;`, `loading_state.tunnel.powerBase`, 0, 1, 0.01, false),
+			new PropertySlider(0.01, 0.14 + (4 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `power`, `loading_state.tunnel.powerBase = value;`, `loading_state.tunnel.powerBase`, 0, 1, 0.01, false),
 
 			//tile properties
-			new PropertySlider(0.01, 0.14 + (5 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `tile size`, `loading_state.tunnel.tileSize = value; world_camera.targetY = value / 2;`, `loading_state.tunnel.tileSize`, 10, 220, 5, true),
-			new PropertySlider(0.01, 0.14 + (6 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion / 2, `tiles / side`, `loading_state.tunnel.tilesPerSide = value;`, `loading_state.tunnel.tilesPerSide`, 1, 8, 1, true),
-			new PropertySlider(0.01, 0.14 + (7 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `sides`, `loading_state.tunnel.sides = value;`, `loading_state.tunnel.sides`, 3, 50, 1, true),
+			new PropertySlider(0.01, 0.14 + (6 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `tile size`, `loading_state.tunnel.tileSize = value; world_camera.targetY = value / 2;`, `loading_state.tunnel.tileSize`, 10, 220, 5, true),
+			new PropertySlider(0.01, 0.14 + (7 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion / 2, `tiles / side`, `loading_state.tunnel.tilesPerSide = value;`, `loading_state.tunnel.tilesPerSide`, 1, 8, 1, true),
+			new PropertySlider(0.01, 0.14 + (8 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), editor_sliderProportion, `sides`, `loading_state.tunnel.sides = value;`, `loading_state.tunnel.sides`, 3, 50, 1, true),
 
 			//tunnel name
-			new PropertyTextBox(0.01, 0.14 + (9 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), `id~`, `loading_state.tunnel.id = value;`, `loading_state.tunnel.id`, `New Tunnel Name: `, false),
+			new PropertyTextBox(0.01, 0.14 + (10 * editor_sliderHeight), editor_propertyMenuWidth - (editor_sliderMargin * 2), `id~`, `loading_state.tunnel.id = value;`, `loading_state.tunnel.id`, `New Tunnel Name: `, ``, false),
 			
 			//save / load
-			new PropertyTextBox(0.01, 0.14 + (11 * editor_sliderHeight), (editor_propertyMenuWidth / 2) - editor_sliderMargin, `load`, `loading_state.tunnel = new Tunnel_FromData(value); loading_state.tunnel.theta = (Math.PI * 2) - world_camera.targetTheta; loading_state.tunnel.updatePosition(0, 0, 0);`, `""`, `Input Tunnel Data: `, true),
-			new PropertyTextBox(0.01 + (editor_propertyMenuWidth / 2), 0.14 + (11 * editor_sliderHeight), (editor_propertyMenuWidth / 2) - editor_sliderMargin, `save`, ``, `""`, `Don't type in the box, just copy this string: \${loading_state.tunnel.giveStringData();}`, true),
+			new PropertyTextBox(0.01, 0.14 + (11 * editor_sliderHeight), (editor_propertyMenuWidth / 2) - editor_sliderMargin, `load`, `loading_state.tunnel = new Tunnel_FromData(value); loading_state.tunnel.theta = (Math.PI * 2) - world_camera.targetTheta; loading_state.tunnel.updatePosition(0, 0, 0);`, `""`, `Input Tunnel Data: `, ``, true),
+			new PropertyTextBox(0.01 + (editor_propertyMenuWidth / 2), 0.14 + (11 * editor_sliderHeight), (editor_propertyMenuWidth / 2) - editor_sliderMargin, `save`, ``, `""`, `Don't type in the box, just copy this string:`, `loading_state.tunnel.giveStringData();`, true),
 		];
+		
+		this.topDarkButton = 2;
 	}
 
 	execute() {
 		this.tunnel.power = this.tunnel.powerBase;
-
-		
 
 		//world thoingies
 		drawSky(color_bg);
 		editor_active = true;
 		this.tunnel.tick();
 		drawPlayerWithParent();
-		this.selectedTileExtra.beDrawn();
 		editor_active = false;
 
 		//side bar
