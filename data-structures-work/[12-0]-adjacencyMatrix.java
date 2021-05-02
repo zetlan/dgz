f59b867c2424d2d57c5dc8117fdd156a95e505c6
BobbyClementine <davis@zetlan.net>
2021-05-02 12:50:07 -0400

    Added file

---
+++ data-structures-work/[12-0]-adjacencyMatrix.java
@@ -0,0 +1,52 @@
+// Name: [REDACTED]
+// Date: May-2-2021
+ 
+import java.util.*;
+import java.io.*;
+
+/* Resource classes and interfaces
+ * for use with Graph0 AdjMat_0_Driver,
+ *              Graph1 WarshallDriver,
+ *          and Graph2 FloydDriver
+ */
+
+interface AdjacencyMatrix
+{
+   void addEdge(int source, int target);
+   void removeEdge(int source, int target);
+   boolean isEdge(int from, int to);
+   String toString();   //returns the grid as a String
+   int edgeCount();
+   List<Integer> getNeighbors(int source);
+   //public List<String> getReachables(String from);  //Warshall extension
+}
+
+interface Warshall      //User-friendly functionality
+{
+   boolean isEdge(String from, String to);
+   Map<String, Integer> getVertices();     
+   void readNames(String fileName) throws FileNotFoundException;
+   void readGrid(String fileName) throws FileNotFoundException;
+   void displayVertices();
+   void allPairsReachability();  // Warshall's Algorithm
+}
+
+interface Floyd
+{
+   int getCost(int from, int to);
+   int getCost(String from, String to);
+   void allPairsWeighted(); 
+}
+
+public class AdjMat implements AdjacencyMatrix//,Warshall//,Floyd 
+{
+   private int[][] grid = null;   //adjacency matrix representation
+   private Map<String, Integer> vertices = null;   // name maps to index (for Warshall & Floyd)
+   /*for Warshall's Extension*/  ArrayList<String> nameList = null;  //reverses the map, index-->name
+	  
+   /*  enter your code here  */  
+   public AdjMat() {
+   
+   }
+ 
+}
