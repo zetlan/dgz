119d8902956d00df1261769a7e383916fc70c8c1
BobbyClementine <davis@zetlan.net>
2021-05-03 09:50:34 -0400

    Finished file

--- data-structures-work/[12-0]-adjacencyMatrix.java
+++ data-structures-work/[12-0]-adjacencyMatrix.java
@@ -45,8 +45,69 @@ public class AdjMat implements AdjacencyMatrix//,Warshall//,Floyd
    /*for Warshall's Extension*/  ArrayList<String> nameList = null;  //reverses the map, index-->name
 	  
    /*  enter your code here  */  
-   public AdjMat() {
-   
+   public AdjMat(int size) {
+      this.grid = new int[size][size];
+      //populate grid with 0s
+      for (int column=0; column<this.grid.length; column++) {
+         for (int row=0; row<this.grid[column].length; row++) {
+            this.grid[column][row] = 0;
+         }
+      }
    }
- 
+
+   //toggles an edge on/off in the graph
+   public void toggleEdge(int node, int neighborToToggle) {
+      this.grid[node][neighborToToggle] = (this.grid[node][neighborToToggle] + 1) % 2;
+   }
+
+   //returns the number of edges in the graph
+   public int edgeCount() {
+      int numEdges = 0;
+      for (int a=0; a<this.grid.length; a++) {
+         for (int b=0; b<this.grid[a].length; b++) {
+            //0 represents no edge, and the only other option is 1, so I can get away with this
+            numEdges += this.grid[a][b];
+         }
+      }
+      return numEdges;
+   }
+
+   //my soul weeps
+   public void addEdge(int node, int neighbor) {
+      this.grid[node][neighbor] = 1;
+   }
+
+   public void removeEdge(int node, int neighbor) {
+      this.grid[node][neighbor] = 0;
+   }
+
+   public boolean isEdge(int node, int neighbor) {
+      return (this.grid[node][neighbor] == 1);
+   }
+
+   public List<Integer> getNeighbors(int source) {
+      //loop through the source line and add all neighbors to the list
+      ArrayList<Integer> toReturn = new ArrayList<Integer>();
+      for (int v=0; v<this.grid[source].length; v++) {
+         if (this.grid[source][v] == 1) {
+            //if there's a neighbor, add to the list
+            toReturn.add(v);
+         }
+      }
+
+      return toReturn;
+   }
+
+
+   public String toString() {
+      String finalStr = "";
+      //loop loop
+      for (int a=0; a<this.grid.length; a++) {
+         for (int b=0; b<this.grid[a].length; b++) {
+            finalStr += " " + this.grid[a][b];
+         }
+         finalStr += "\n";
+      }
+      return finalStr;
+   }
 }
