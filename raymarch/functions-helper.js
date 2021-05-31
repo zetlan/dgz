ca64b9df0133c04f528c1961e4356e08744844ff
BobbyClementine <davis@zetlan.net>
2021-05-31 17:42:58 -0400

    Added vector functions

--- raymarch/functions-helper.js
+++ raymarch/functions-helper.js
@@ -6,12 +6,16 @@ INDEX
 
 */
 
+
+
+
 //for quaternion functions, quaternions are expected to be an array of length 4, with the structure [w, x, y, z].
 function quatNormalize(quat) {
 	var magnitude = Math.sqrt(quat[0] * quat[0] + quat[1] * quat[1] + quat[2] * quat[2] + quat[3] * quat[3]);
 	return [quat[0] / magnitude, quat[1] / magnitude, quat[2] / magnitude, quat[3] / magnitude];
 }
 
+//ack this is a mess
 function quatMultiply(quat1, quat2) {
 	return [
 		quat1[0] * quat2[0] - quat1[1] * quat2[1] - quat1[2] * quat2[2] - quat1[3] * quat2[3],
@@ -21,6 +25,47 @@ function quatMultiply(quat1, quat2) {
 	]
 }
 
+//converts [w, x, y, z] to [x, y, z, theta]
+function quatToAxisAngle(quat) {
+
+}
+
 function quatToCart(quat) {
-	
+
+}
+
+//functions that apply to vectors
+function vAdd(vec1, vec2) {
+	var newVec = [];
+	//go backwards so the array only needs to be lengthened once
+	for (var n=vec1.length-1; n>-1; n--) {
+		newVec[n] = vec1[n] + vec2[n];
+	}
+	return newVec;
+}
+
+//returns the dot product of two matching vectors
+function vDot(vec1, vec2) {
+	var sum = 0;
+	for (var n=0; n<vec1.length; n++) {
+		sum += vec1[n] * vec2[n];
+	}
+	return sum;
+}
+
+//returns the length of a vector
+function vLen(vector) {
+	var midPoint = 0;
+	vector.forEach(v => {
+		midPoint += v * v;
+	});
+	return Math.sqrt(midPoint);
+}
+
+function vSub(vec1, vec2) {
+	var newVec = [];
+	for (var n=vec1.length-1; n>-1; n--) {
+		newVec[n] = vec1[n] - vec2[n];
+	}
+	return newVec;
 }
