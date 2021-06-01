83c234c21ad7f34af25783c5d6445ca4eda1090b
BobbyClementine <davis@zetlan.net>
2021-06-01 16:07:34 -0400

    Moved clipping function to common

--- run-3-clone/functions-coordinate.js
+++ run-3-clone/functions-coordinate.js
@@ -4,7 +4,6 @@
 	cameraToScreen(point);
 	cartToPol(x, y, z);
 	clipToPlane(polyPoints, tolerance, planePoint, planeNormal);
-	clipToZ0(polyPoints, tolerance, invertClipDirection);
 	dirToTunnelCenter(tunnel, x, y, z);
 	getDistance(obj1, obj2);
 	getDistance2d(xyP1, xyP2);
@@ -84,84 +83,6 @@ function clipToPlane(polyPoints, tolerance, planePoint, planeNormal) {
 		polyPoints[p] = relativeToSpace(polyPoints[p], planePoint, planeNormal);
 	}
 
-	return polyPoints;
-}
-
-function clipToZ0(polyPoints, tolerance, invertClipDirection) {
-	//to save time, inverting the clip direction just means inverting all the points, then inverting back
-	if (invertClipDirection) {
-		polyPoints.forEach(p => {
-			p[2] *= -1;
-		});
-	}
-	//make all the polypoints have a boolean that says whether they're clipped or not, this is used for number of neighbors
-	var halt = true;
-	var n = 0;
-	while (halt && n < polyPoints.length - 1) {
-		if (polyPoints[n][2] < tolerance) {
-			polyPoints[n][3] = true;
-		} else {
-			halt = false;
-			polyPoints[n][3] = false;
-		}
-		n += 1;
-	}
-
-	//return early if all points are destroyed
-	if (halt) {
-		return [];
-	}
-
-	while (n < polyPoints.length) {
-		polyPoints[n][3] = polyPoints[n][2] < tolerance;
-		n += 1;
-	}
-	for (var y=0; y<polyPoints.length; y++) {
-		//if the selected point will be clipped, run the algorithm
-		if (polyPoints[y][3]) {
-			//decide what to do based on the number of adjacent non-clipped points
-			switch (!polyPoints[(y+(polyPoints.length-1))%polyPoints.length][3] + !polyPoints[(y+1)%polyPoints.length][3]) {
-				case 0:
-					//if there are no free friends, there's no point in attempting, so just move on
-					polyPoints.splice(y, 1);
-					y -= 1;
-					break;
-				case 1:
-					//determine which one is free, then move towards it
-					var friendCoords;
-					var moveAmount;
-					//lesser friend
-					if (!polyPoints[(y+(polyPoints.length-1))%polyPoints.length][3]) {
-						friendCoords = polyPoints[(y+(polyPoints.length-1))%polyPoints.length];
-						moveAmount = getPercentage(friendCoords[2], polyPoints[y][2], tolerance);
-						polyPoints[y] = [linterp(friendCoords[0], polyPoints[y][0], moveAmount), linterp(friendCoords[1], polyPoints[y][1], moveAmount), tolerance, true];
-					} else {
-						//greater friend
-						friendCoords = polyPoints[(y+1)%polyPoints.length];
-						moveAmount = getPercentage(friendCoords[2], polyPoints[y][2], tolerance);
-						polyPoints[y] = [linterp(friendCoords[0], polyPoints[y][0], moveAmount), linterp(friendCoords[1], polyPoints[y][1], moveAmount), tolerance, true];
-					}
-					break;
-				case 2:
-					//move towards both friends
-					var friendCoords = polyPoints[(y+(polyPoints.length-1))%polyPoints.length];
-					var moveAmount = getPercentage(friendCoords[2], polyPoints[y][2], tolerance);
-					polyPoints.splice(y, 0, [linterp(friendCoords[0], polyPoints[y][0], moveAmount), linterp(friendCoords[1], polyPoints[y][1], moveAmount), tolerance, true]);
-					y += 1;
-
-					friendCoords = polyPoints[(y+1)%polyPoints.length];
-					moveAmount = getPercentage(friendCoords[2], polyPoints[y][2], tolerance);
-					polyPoints[y] = [linterp(friendCoords[0], polyPoints[y][0], moveAmount), linterp(friendCoords[1], polyPoints[y][1], moveAmount), tolerance, true];
-					y -= 1;
-					break;
-			}
-		}
-	}
-	if (invertClipDirection) {
-		polyPoints.forEach(p => {
-			p[2] *= -1;
-		});
-	}
 	return polyPoints;
 }
 
