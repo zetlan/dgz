1987cde3332383c6a833924f80fcf8e9eb914310
BobbyClementine <davis@zetlan.net>
2021-06-03 16:52:08 -0400

    Created file

---
+++ mandelbrotCPP/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//using namespace std;
+using std::cout;
+using std::cin;
+using std::string;
+
+
+//int main(int argc, const char * argv[]) { ???
+/*
+int main(int argc, char** argv) {
+    
+} */
+
+
+
+int main() {
+    cout << "enter a number \n\n";
+    
+    int storage;
+    string esc = "You see the image of the smiling sun overhead, its reflection scattered among the plexiglass buildings\nwritten on one of t";
+    cin >> storage;
+    cout << storage << " is your number. Keep it. I do not want it. I have too many already.\n";
+    //String temp = std::in;
+    return storage;
+}
