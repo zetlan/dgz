fa85752b52fa6c54e758a7ec4a600d6677d4ec93
BobbyClementine <davis@zetlan.net>
2021-06-04 22:06:29 -0400

    Initial

---
+++ writings/poems/testing123.js
@@ -0,0 +1,43 @@
+var poems_monsters = `
+I'm scared of monsters.<br>
+Monsters that abduct children, that steal dreams, that hide in the shadows.<br>
+Monsters that are overly tall, with cracking joints,<br>
+and monsters that whisper sweet outrages, that ask for matches, that dress themselves.<br>
+I'm scared of monsters that call for unity, and choose to slice populations <br>
+because in the end, I am a simple coward, but they aren't afraid to wield power. <br>
+`;
+
+var poems_abigail = `
+Abigail, Ellie, Stephanie, I look at them and I feel.. disgust.<br>
+How sad, how poetic, that I spend my days decrying those who oppose them and yet ultimately, I am the same? <br>
+A tangled ball of nightmares and invalidity, like a million insects crawling over my skin. It's horrible. <br>
+It's me.<br>
+`
+
+var poems_fooled = `
+Did I do it? Did I fool you? Did you manage to think these were actual poems? No, no you absolute fool. This was about me the whole time. <br>
+A withering tangle of narcissism, how dare you think your thoughts are important enough to put on a website? You absolute limp-wristed piece of cutlery. You think other people want to read what's going on in that messed up head of yours? <br>
+You think you aren't going to face consequences for this?<br>
+<br>
+In the end, it's still all about me. I'm the narrator, I'm the critic, I'm the coward <strong>and</strong> the knight in shining armor, wielding a keyboard and the power of creation. And with that, I carve out a tiny trash can in the corner of the universe.<br>
+Pathetic.`;
+
+var poems_vague = `
+I sometimes wonder if being intentionally vague is problematic. Not just in the instance of using it as a crutch too much, but <br>
+well yeah that part is rather important, but like<br>
+wouldn't it be better to say something specific for once? Instead of talking medieval metaphors, actually say something true? Something honest, that's not hidden and poisoned by the need to convey strength?<br>
+<br>
+<br>
+It's currently spring. It's going to be summer soon, and that means warmer weather, which in turn means shorts and t-shirts, instead of more covering clothing. <br>
+This year, I have been tasked with buying my own clothing, which to anyone else would be a great liberty, but to me is a burden. I get to peruse the poorly organized clothing store websites as they try to sell me everything except what I actually want to buy, which is simple shorts that fit me and don't make me hate myself when I wear them.<br>
+On the other hand, I could attempt to buy the clothing that I <strong>actually <i>actually</i></strong> want to buy, but that's too real even for this website. I have limitations with a repository that people I am close to can see. Unfortunate.<br>
+<br>
+I've hit my limit. How to get to the crux of the issue, without veering off into basic teenager territory, and without spiraling into something that could get me a good talking to by my family? It's impossible. So vagueposting it is.
+`
+
+var poems_who = `
+Cement? A-16? Shrey? Is that you?
+Not going to lie, it's terrifying not knowing who could be reading this. Y'all could be reading this right now and simply.. decide to not tell me. 
+That's fair. I wouldn't tell me either. But still, I suppose I'm sorry. These writings.. do not depict the best sides of me. That's why I don't have them on the main page.
+Feel free to ask me any questions, I'll probably be more open about them in discord logs than I am here.
+`
