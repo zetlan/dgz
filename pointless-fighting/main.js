4b92290f1919bdf54931262f922f899802ae3fd1
BobbyClementine <davis@zetlan.net>
2021-07-17 10:04:45 -0400

    Created files for development

---
+++ pointless-fighting/main.js
@@ -0,0 +1,27 @@
+//events
+
+
+//global vars
+var cornerCoords = [0, 0, 20, 15];
+
+const color_sword = "#32688A";
+
+
+//main functions
+function setup() {
+
+}
+
+function main() {
+	//draw players
+
+	//draw map
+}
+
+function handleKeyPress(a) {
+
+}
+
+function handleKeyNegate(a) {
+	
+}
