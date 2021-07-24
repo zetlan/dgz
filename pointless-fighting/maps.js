b583cffbe4ae5a5dd5942a548de7feefee92858c
BobbyClementine <davis@zetlan.net>
2021-07-24 13:45:53 -0400

    Added functions to parse txt data

--- pointless-fighting/maps.js
+++ pointless-fighting/maps.js
@@ -6,7 +6,7 @@ let map_demo2;
 
 function maps_load() {
 	//access file
-	fetch('./maps.txt').then(response => response.text()).then((data) => {
+	fetch('./maps.txt').then(r => r.text()).then((data) => {
 		//different spacing determines which are maps and which are connections
 		var parts = data.split("\n\n\n");
 		var maps = parts[0].split("\n\n");
@@ -17,30 +17,80 @@ function maps_load() {
 
 		//turn the connections into.. actual connections
 		maps_connect(connections);
-	})
+
+		//player stuff
+		loading_map = world_maps["start"];
+		loading_map.entities.push(player);
+		player.map = loading_map;
+	});
 }
 
 
 
 function maps_create(data) {
+	//seperate by double newline
+	data.forEach(m => {
+		var splitTag = m.split("\n");
+		//the first line is always the name + optional starting coordinates
+		var name = splitTag[0].split("[")[0];
+		var coords = "[" + splitTag[0].split("[")[1];
+		if (coords == "[undefined") {
+			coords = [0, 0];
+		} else {
+			try {
+				coords = JSON.parse(coords);
+			} catch (er) {
+				console.error(`was unable to create coordinates for data ${coords}`)
+			}
+		}
+		splitTag.splice(0, 1);
+		
+		//create the actual zone
+		var theZone = new Zone();
+		theZone.x = coords[0];
+		theZone.y = coords[1];
+		theZone.name = name;
+		theZone.data = splitTag;
+		world_maps[name] = theZone;
 
+	});
 }
 
 function maps_connect(data) {
-	data = data.split("\n");
-
 	data.forEach(d => {
-		var splitTag = d.split("\n");
+		var splitTag = d.split("|");
 		//part 1 is start map, part 2 is end map, part 3 is offset
 		try {
-			var startMap = eval(`map_${splitTag[0]}`);
-			var endMap = eval(`map_${splitTag[1]}`);
+			var startMap = world_maps[splitTag[0]];
+			var endMap = world_maps[splitTag[1]];
 			var coordOffset = JSON.parse(splitTag[2]);
-			startMap.connections.push([endMap, coordOffset]);
-			console.log(startMap.connections);
+			if (endMap != undefined) {
+				startMap.connections.push([endMap, coordOffset]);
+			} else {
+				console.log(`was unable to make connection to map ${splitTag[1]}`);
+			}
+			
 		} catch (er) {
 			console.log(`was unable to make connection with data "${d}"!`);
 		}
-		
 	});
 }
+
+
+
+
+
+
+
+function outputConnections() {
+	
+}
+
+function outputMap() {
+	//output the loading map, with all its connections
+}
+
+function outputWorld() {
+
+}
+
