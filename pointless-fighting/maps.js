6c9a4efc9a2e46eb1d682606fd3b333c76fdd32f
BobbyClementine <davis@zetlan.net>
2021-08-03 18:19:41 -0400

    Improved import/export of maps

--- pointless-fighting/maps.js
+++ pointless-fighting/maps.js
@@ -1,62 +1,14 @@
+/*
+INDEX:
+	importConnections(data);
+	importData(arr, character, data);
+	importZone(zoneLineString);
 
-let map_start;
-let map_demo1;
-let map_demo2;
+	maps_load();
 
+*/
 
-function maps_load() {
-	//access file
-	fetch('./maps.txt').then(r => r.text()).then((data) => {
-		//different spacing determines which are maps and which are connections
-		var parts = data.split("\n\n\n");
-		var maps = parts[0].split("\n\n");
-		var connections = parts[1].split("\n");
-
-		//turn the maps into actual maps
-		maps_create(maps);
-
-		//turn the connections into.. actual connections
-		maps_connect(connections);
-
-		//player stuff
-		loading_map = world_maps["start"];
-		loading_map.entities.push(player);
-		player.map = loading_map;
-	});
-}
-
-
-
-function maps_create(data) {
-	//seperate by double newline
-	data.forEach(m => {
-		var splitTag = m.split("\n");
-		//the first line is always the name + optional starting coordinates
-		var name = splitTag[0].split("[")[0];
-		var coords = "[" + splitTag[0].split("[")[1];
-		if (coords == "[undefined") {
-			coords = [0, 0];
-		} else {
-			try {
-				coords = JSON.parse(coords);
-			} catch (er) {
-				console.error(`was unable to create coordinates for data ${coords}`)
-			}
-		}
-		splitTag.splice(0, 1);
-		
-		//create the actual zone
-		var theZone = new Zone();
-		theZone.x = coords[0];
-		theZone.y = coords[1];
-		theZone.name = name;
-		theZone.data = splitTag;
-		world_maps[name] = theZone;
-
-	});
-}
-
-function maps_connect(data) {
+function importConnections(data) {
 	data.forEach(d => {
 		var splitTag = d.split("|");
 		//part 1 is start map, part 2 is end map, part 3 is offset
@@ -76,21 +28,167 @@ function maps_connect(data) {
 	});
 }
 
+//takes in string data and converts it into positions in the array
+function importData(arr, character, data) {
+	//first split the data into binary existance
+	var expandedData = ``;
+	for (var g=0; g<data.length; g++) {
+		expandedData += tileImage_map[data[g]].toString(2).padStart(6, 0);
+	}
 
-
-
-
-
-
-function outputConnections() {
-	
+	//map binary data onto array
+	var aWidth = arr[0].length;
+	for (var c=0; c<expandedData.length; c++) {
+		if (expandedData[c] == "1") {	
+			arr[Math.floor(c / aWidth)][c % aWidth] = character;
+		}
+	}
 }
 
-function outputMap() {
-	//output the loading map, with all its connections
+function importZone(zoneLineString) {
+	var returnZone = new Zone();
+	//first split by tag
+	var splitTag = zoneLineString.split("|");
+
+	//parse tags
+	splitTag.forEach(s => {
+		var superSplit = s.split("~");
+		switch (superSplit[0]) {
+			case "id":
+				returnZone.name = superSplit[1];
+				break;
+			case "coords":
+				returnZone.x = superSplit[1] * 1;
+				returnZone.y = superSplit[2] * 1;
+				break;
+			case "dims":
+				returnZone.data = [];
+				returnZone.display = [];
+				//y size
+				for (var b=0; b<superSplit[2]*1; b++) {
+					returnZone.data.push([]);
+					returnZone.display.push([]);
+					for (var a=superSplit[1]*1 - 1; a>=0; a--) {
+						returnZone.data[b][a] = " ";
+						returnZone.display[b][a] = " ";
+					}
+				}
+				break;
+			case "ground":
+				importData(returnZone.data, superSplit[1], superSplit[2]);
+				break;
+			case "display":
+				//update display array
+				var char = 0;
+				while (char < superSplit[1].length) {
+					returnZone.display[Math.floor(char/returnZone.display[0].length)][char%returnZone.display[0].length] = superSplit[1][char];
+					char += 1;
+				}
+				break;
+		}
+	});
+
+
+	//add zone to world
+	world_maps[returnZone.name] = returnZone;
 }
 
-function outputWorld() {
 
+
+
+function maps_load() {
+	//access file
+	fetch('./maps.txt').then(r => r.text()).then((data) => {
+		//different spacing determines which are maps and which are connections
+		var parts = data.split("\n\n\n");
+		var maps = parts[0].split("\n\n");
+		var connections = parts[1].split("\n");
+
+		//turn the maps into actual maps
+		maps.forEach(m => {
+			importZone(m);
+		});
+
+		//turn the connections into.. actual connections
+		importConnections(connections);
+
+		//player stuff
+		loading_map = world_maps["start"];
+		loading_map.entities.push(player);
+		player.map = loading_map;
+	});
+}
+
+
+
+
+
+
+
+function exportData(data) {
+	var finalString = ``;
+	var typeString = ``;
+	var buffer1 = "";
+	for (var type=0; type<10; type++) {
+		for (var y=0; y<data.length; y++) {
+			for (var x=0; x<data.length; x++) {
+				buffer1 += "" + (1 * (data[y][x] === "" + type));
+
+				//if buffer1 is long enough, convert to a character
+				if (buffer1.length > 5) {
+					typeString += tileImage_key[Number.parseInt(buffer1, 2)];
+					buffer1 = "";
+				}
+			}
+		}
+
+		//remove lagging 0s
+		var lagging0s = 0;
+		while (typeString[typeString.length-1-lagging0s] === "0") {
+			lagging0s += 1;
+		}
+		typeString = typeString.substring(0, typeString.length - lagging0s);
+
+		//add final string
+		if (typeString.length > 0) {
+			finalString += `|ground~${type}~${typeString}`;
+			typeString = ``;
+		}
+	}
+
+	//return final string
+	finalString = finalString.substring(1);
+	return finalString;
+}
+
+function exportZone(zoneObj) {
+	var toReturn = ``;
+	//standard properties
+	toReturn += `id~${zoneObj.name}`;
+	toReturn += `|coords~${zoneObj.x}~${zoneObj.y}`;
+	toReturn += `|dims~${zoneObj.data[0].length}~${zoneObj.data.length}`;
+
+	//collision data
+	toReturn += `|${exportData(zoneObj.data)}`;
+
+	//image data
+	toReturn += `|display~`;
+	for (var y=0; y<zoneObj.display.length; y++) {
+		for (var x=0; x<zoneObj.display[0].length; x++) {
+			toReturn += zoneObj.display[y][x];
+		}
+	}
+
+	return toReturn;
+}
+
+function exportConnections() {
+	var toReturn = ``;
+	Object.values(world_maps).forEach(z => {
+		z.connections.forEach(c => {
+			toReturn += `${z.name}|${c[0].name}|[${c[1][0]}, ${c[1][1]}]\n`;
+		});
+	});
+	return toReturn;
 }
 
