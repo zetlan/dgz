9d8e78c36725410aec483fa395ebfe5fab2308d0
BobbyClementine <davis@zetlan.net>
2021-08-04 09:07:30 -0400

    Misc. changes for development

--- pointless-fighting/functions-helper.js
+++ pointless-fighting/functions-helper.js
@@ -1,7 +1,40 @@
+/*
+INDEX
+	generateTileImageMap();
+	getImage(url);
+	polToXY(startX, startY, angle, magnitude);
+	spaceToScreen(x, y);
+	screenToSpace(x, y);
 
-function editor_selectExit() {
-	var exit = Math.floor(((cursor_y / canvas.height) - 0.21) / 0.04);
-	if (loading_map.connections[exit] != undefined) {
-		editor_exitSelected = exit;
+*/
+
+
+
+
+function generateTileImageMap() {
+	var map = {};
+	for (var c=0; c<tileImage_key.length; c++) {
+		map[tileImage_key[c]] = c;
 	}
+	return map;
+}
+
+function getImage(url) {
+	var image = new Image();
+	image.src = url;
+	return image;
+}
+
+function polToXY(startX, startY, angle, magnitude) {
+	var xOff = magnitude * Math.cos(angle);
+	var yOff = magnitude * Math.sin(angle);
+	return [startX + xOff, startY + yOff];
+}
+
+function spaceToScreen(x, y) {
+	return [(x - camera.cornerCoords[0]) * camera.scale, (y - camera.cornerCoords[1]) * camera.scale * camera.vSquish];
+}
+
+function screenToSpace(x, y) {
+	return [(x / camera.scale) + camera.cornerCoords[0], (y / camera.scale / camera.vSquish) + camera.cornerCoords[1]];
 }
