72a19abaae4f5a917f7ba8990c636391a75b59a7
BobbyClementine <davis@zetlan.net>
2021-09-08 15:53:20 -0400

    Added localStorage support

--- circles/functions-helper.js
+++ circles/functions-helper.js
@@ -1,3 +1,40 @@
+
+function determineTutorialText() {
+	if (game_mode == 0) {
+		//player knows how to move
+		if (menu_x != 0 || menu_y != 0) {
+			tutorial.hasDone[0] = true;
+		}
+	} else {
+		if (scan_time > 0) {
+			tutorial.hasDone[1] = true;
+		}
+	}
+
+	//determine which text to display
+	if (game_mode == 0) {
+		if (!tutorial.hasDone[0]) {
+			if (game_time > 600) {
+				text_buffer = tutorial.texts[0];
+				text_time = text_time_static;
+			}
+		} else if (!tutorial.hasDone[2]) {
+			var cds = menu_queue[menu_queue.length-1];
+			if (menu_nodeStruct[cds[1]][cds[0]].completed == true) {
+				text_buffer = tutorial.texts[2];
+				text_time = text_time_static;
+			}
+		}
+	} else {
+		//get minimum distance to orb, if it's large enough do the scan prompt
+		if (!tutorial.hasDone[1] && !orbsAreAllBounced() && !orbsAreOnScreen()) {
+			text_buffer = tutorial.texts[1];
+			text_time = text_time_static;
+		}	
+	}
+}
+
+
 //interpolates between two values, with an exponential increase towards the second one
 function droperp(a, b, percentage) {
 	return linterp(a, b, Math.pow(percentage, 15));
@@ -15,6 +52,39 @@ function loadMenu(success) {
 	camera.y = camera_menuCoords[1];
 	transitionModeGoal = 0;
 	transitionSpeed = transitionSpeedConstant;
+}
+
+function localStorage_read() {
+	var toRead = window.localStorage["circles_data"];
+	try {
+		toRead = JSON.parse(toRead);
+	} catch (error) {
+		console.log(`ERROR: could not parse ${toRead}, using default`);
+		return;
+	}
+
+	//make sure it's somewhat safe, and then make it into the game flags
+	if (typeof(toRead) == "object") {
+		data_persistent = toRead;
+	} else {
+		console.log("ERROR: invalid type specified in save data, using default");
+		return;
+	}
+}
+
+function localStorage_write() {
+	data_persistent.res = levels_st.statics[1].orbOut;
+	data_persistent.tut = tutorial.hasDone[0] && tutorial.hasDone[1] && tutorial.hasDone[2];
+	data_persistent.vol = levels_st.statics[2].orbOut;
+	//update solve level
+	for (var y=0; y<menu_levelOrder.length; y++) {
+		if (menu_levelOrder[y].completed) {
+			data_persistent.lvl = Math.max(data_persistent.lvl, y);
+		}
+	}
+
+	//write persistent data to localStorage
+	window.localStorage["circles_data"] = JSON.stringify(data_persistent);
 }
 
 //handles move inputs in the menu
@@ -93,6 +163,13 @@ function scanWorld() {
 	scan_time = scan_time_static;
 }
 
+function setLevelProgress(number) {
+	//sets all the levels before a certain point in the level array to completed
+	for (var l=0; l<menu_levelOrder.length; l++) {
+		menu_levelOrder[l].completed = (l <= number);
+	}
+}
+
 
 function screenToSpace(x, y) {
 	return [((x - (canvas.width / 2)) / camera.scale) + camera.x, ((y - (canvas.height / 2)) / camera.scale) + camera.y];
@@ -100,4 +177,29 @@ function screenToSpace(x, y) {
 
 function spaceToScreen(x, y) {
 	return [((x - camera.x) * camera.scale) + canvas.width / 2, ((y - camera.y) * camera.scale) + canvas.height / 2];
+}
+
+function trueReset() {
+	if (confirm("This action cannot be undone. Would you like to reset completely? \n(Press OK to reset, Cancel to not)")) {
+		data_persistent = undefined;
+		window.localStorage["circles_data"] = undefined;
+		window.location.reload();
+	}
+}
+
+function updateSettings() {
+	var stStore = levels_st.statics;
+	stStore[1].orbOut = data_persistent.res;
+	stStore[1].orbs[data_persistent.res].layersCurrent = 0;
+	stStore[1].func(stStore[1].values[data_persistent.res]);
+
+	stStore[2].orbOut = data_persistent.vol;
+	stStore[2].orbs[data_persistent.vol].layersCurrent = 0;
+	stStore[2].func(stStore[2].values[data_persistent.vol]);
+
+	tutorial.hasDone[0] = data_persistent.tut;
+	tutorial.hasDone[1] = data_persistent.tut;
+	tutorial.hasDone[2] = data_persistent.tut;
+
+	setLevelProgress(data_persistent.lvl);
 }
