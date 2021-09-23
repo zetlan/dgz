1617b35bf13d34a622c4271cd7e2011ca27e4351
BobbyClementine <davis@zetlan.net>
2021-09-23 09:38:06 -0400

    Created

---
+++ AI-work/word-ladders/word-ladders.py
@@ -0,0 +1,36 @@
+#NAME: [REDACTED]
+#PERIOD: 1
+#DATE: September-23-2021
+
+fileNameWORDS = "words_06_letters.txt"
+fileNamePUZZLE = "puzzles_normal.txt"
+
+
+
+
+"""
+specifications:
+1. accept 2 arguments (first is the name of the file containing words, second is the name of the file containing ladder puzzles)
+
+2. read the words in from the file, make a dictionary, print time taken to create structure
+
+3. read puzzles from the word ladder file and solve them witht he shortest path, ladders may be impossible!
+
+4. display total time taken after solving all puzzles
+
+5. total time must be less than two minutes
+
+
+
+
+questions: 
+1. how many words are singletons?
+
+2. how many words are in the largest interconnected spot?
+
+3a. how many clumps are there?
+
+3b. how many clumps (excluding singletons and excluding large clump) are there?
+
+4. what is the longest ideal path? and print that path!
+"""
