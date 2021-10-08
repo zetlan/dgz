4ddf695f9acf0edd452c1a49bebf61082cc7adc3
BobbyClementine <davis@zetlan.net>
2021-10-08 08:54:23 -0400

    Added file for organization

---
+++ chip-net/functions-drawing.js
@@ -0,0 +1,9 @@
+
+//sets default canvas states
+function setCanvasPreferences() {
+	ctx.lineWidth = 2;
+	ctx.lineJoin = "round";
+	ctx.lineCap = "square";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "center";
+}
