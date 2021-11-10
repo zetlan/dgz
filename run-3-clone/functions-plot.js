909f6d8881e2be257d34946c5f93652b5fd6d7d1
BobbyClementine <davis@zetlan.net>
2021-11-10 10:47:36 -0500

    Added cutscene tree handling and improved triggers

--- run-3-clone/functions-plot.js
+++ run-3-clone/functions-plot.js
@@ -1,38 +1,99 @@
 
 /* 
 INDEX:
+	createCutsceneTree();
 	placeOneTimeCutsceneTriggers();
+	placeOneTimeCutscene();
 	trueReset();
 	unlockCharacter(characterName);
 	updatePlotProgression();
 */
 
+//takes it string data for a cutscene tree and creates objects from it
+function createCutsceneTree(stringDat) {
+	var arrDat = stringDat.split("\n");
+	arrDat = arrDat.map(a => [(a.match(/\t/g) || []).length, a.replaceAll("\t", "").split("~")]);
+
+	//go to the first line
+	//create the node for that
+	return createCutsceneTree_recursive(arrDat);
+}
+
+function createCutsceneTree_recursive(strArr) {
+	//make sure there's no reference weirdness going on
+	strArr = JSON.parse(JSON.stringify(strArr));
+
+	//first make every tab number relative to 0
+	var amount = strArr[0][0];
+	strArr.forEach(g => {
+		g[0] -= amount;
+	});
+
+
+	//convert that first line into a node
+	var mainNode = new CNode(+strArr[0][1][1], +strArr[0][1][2], strArr[0][1][0], []);
+
+	//if there are no children just return
+	if (strArr.length < 2) {
+		return mainNode;
+	}
+	
+	//figure out all children
+	var stopIndex = 1;
+	//get all individual child data
+	for (var i=2; i<strArr.length; i++) {
+		if (strArr[i][0] <= 1) {
+			mainNode.children.push(createCutsceneTree_recursive(strArr.slice(stopIndex, i)));
+			stopIndex = i;
+		}
+	}
+
+	//last child
+	mainNode.children.push(createCutsceneTree_recursive(strArr.slice(stopIndex)));
+	return mainNode;
+}
+
+
+
 
 function placeOneTimeCutsceneTriggers() {
 	//triggers
+	placeOneTimeCutscene("Level 1", 0.5, true, "planetMissing");
+	placeOneTimeCutscene("F-4", 1.1, false, "river", true);
+	placeOneTimeCutscene("U-1", 0.2, false, "planetStolen", true);
+	placeOneTimeCutscene("New Tunnel, Part 1", 0.1, false, "selfAssembly", true);
 
-	//overworld
-	getObjectFromID("Level 1").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Level 1"), 0.5, true, "planetMissing"));
-	getObjectFromID("F-4").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("F-4"), 1.1, false, "river", true));
-	getObjectFromID("U-1").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("U-1"), 0.2, false, "planetStolen", true));
-	getObjectFromID("New Tunnel, Part 1").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("New Tunnel, Part 1"), 0.1, false, "selfAssembly", true));
+	//the way back
+	placeOneTimeCutscene("The Way Back, Part 1", 0.5, false, "angelVsBunny", true);
+	placeOneTimeCutscene("The Way Back, Part 8", 0.5, false, "boatRide");
 
-	getObjectFromID("Launch Site A").freeObjs.push(new Tunnel_Blocker(getObjectFromID("Launch Site A"), 6, `3Woooo`));
-	getObjectFromID("Launch Site C").freeObjs.push(new Tunnel_Blocker(getObjectFromID("Launch Site C"), 15, `3ooooo`));
+	placeOneTimeCutscene("Plan A, Part 8", 54.5, false, "wait");
 
+	placeOneTimeCutscene("Plan C, Part 8", 97, false, "superpowers", true);
+	placeOneTimeCutscene("Plan C, Part 9", 110, false, "twoMonthWait1", true);
+	placeOneTimeCutscene("Plan C, Part 12", 86, false, "twoMonthWait2", true);
+	placeOneTimeCutscene("Plan C, Part 14", 69.4, false, "twoMonthWait3", true);
+
+	//the way onwards
+	placeOneTimeCutscene("C-1", 60.5, false, "morningHypothesis1", true);
+	placeOneTimeCutscene("C-2", 84, false, "morningHypothesis2", true);
+	placeOneTimeCutscene("C-3", 99, false, "morningHypothesis3", true);
+	placeOneTimeCutscene("C-4", 97.4, false, "morningHypothesis4", true);
+	placeOneTimeCutscene("C-5", 115.5, false, "morningHypothesis5", true);
+
+
+
+	//achievement triggers
 	getObjectFromID("B-4").freeObjs.push(new CutsceneTrigger_NoJumpWithChar(getObjectFromID("B-4"), ["Runner", "Skater", "Child"], "cheating"));
 	getObjectFromID("N-2").freeObjs.push(new CutsceneTrigger_NoJumpWithChar(getObjectFromID("N-2"), ["Skater"], "skates"));
 
-	//the way back
-	getObjectFromID("The Way Back, Part 1").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("The Way Back, Part 1"), 0.5, false, "angelVsBunny", true));
-	getObjectFromID("The Way Back, Part 8").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("The Way Back, Part 8"), 0.5, false, "boatRide"));
-	
-	getObjectFromID("Plan A, Part 8").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Plan A, Part 8"), 54.5, false, "wait"));
+	//character encounters
+	getObjectFromID("Level 10").freeObjs.push(new StaticCharacterPermanent(getObjectFromID("Level 10"), 12, 58, "Skater", "comingThrough"));
+	getObjectFromID("Level 41").freeObjs.push(new StaticCharacterPermanent(getObjectFromID("Level 41"), 3, 1, "Lizard", "heavySleeper"));
 
-	getObjectFromID("Plan C, Part 8").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Plan C, Part 8"), 97, false, "superpowers", true));
-	getObjectFromID("Plan C, Part 9").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Plan C, Part 9"), 110, false, "twoMonthWait1", true));
-	getObjectFromID("Plan C, Part 12").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Plan C, Part 12"), 86, false, "twoMonthWait2", true));
-	getObjectFromID("Plan C, Part 14").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("Plan C, Part 14"), 69.4, false, "twoMonthWait3", true));
+	//not triggers, but special objects so they get to be here as well
+	getObjectFromID("Launch Site A").freeObjs.push(new Tunnel_Blocker(getObjectFromID("Launch Site A"), 6, `3Woooo`));
+	getObjectFromID("Launch Site C").freeObjs.push(new Tunnel_Blocker(getObjectFromID("Launch Site C"), 15, `3ooooo`));
 
 	//hard-coded values ew
 	var level = getObjectFromID("Level X");
@@ -40,17 +101,11 @@ function placeOneTimeCutsceneTriggers() {
 
 	level = getObjectFromID("Launch Site A");
 	level.freeObjs.push(new Boat(77892, 44, -270906, (Math.PI * 2.5) - level.theta, 0.6));
+}
 
-	//the way onwards
-	getObjectFromID("C-1").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("C-1"), 60.5, false, "morningHypothesis1", true));
-	getObjectFromID("C-2").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("C-2"), 84, false, "morningHypothesis2", true));
-	getObjectFromID("C-3").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("C-3"), 99, false, "morningHypothesis3", true));
-	getObjectFromID("C-4").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("C-4"), 97.4, false, "morningHypothesis4", true));
-	getObjectFromID("C-5").freeObjs.push(new OneTimeCutsceneTrigger(getObjectFromID("C-5"), 115.5, false, "morningHypothesis5", true));
-
-	//character encounters
-	getObjectFromID("Level 10").freeObjs.push(new StaticCharacterPermanent(getObjectFromID("Level 10"), 12, 58, "Skater", "comingThrough"));
-	getObjectFromID("Level 41").freeObjs.push(new StaticCharacterPermanent(getObjectFromID("Level 41"), 3, 1, "Lizard", "heavySleeper"));
+function placeOneTimeCutscene(levelID, levelTilePos, reverseDirection, cutsceneID, isImmersive) {
+	var tunnel = getObjectFromID(levelID);
+	tunnel.freeObjs.push(new OneTimeCutsceneTrigger(tunnel, levelTilePos, reverseDirection, cutsceneID, isImmersive));
 }
 
 function trueReset() {
