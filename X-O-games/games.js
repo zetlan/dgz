cc23f510943d3958c26a505ca465a54c72236e41
BobbyClementine <davis@zetlan.net>
2021-12-19 19:45:22 -0500

    Misc. changes

--- X-O-games/games.js
+++ X-O-games/games.js
@@ -1,41 +1,152 @@
 
 /*basic game rules:
-    play on a rectangular board made of squares
-    boards are represented as a string that is length n, where the board is size p, q and p * q = n
-    two players
-    take turns placing one piece at a time
-    there must be some way to evaluate board utility
+	play on a rectangular board made of squares
+	boards are represented as a string that is length n, where the board is size p, q and p * q = n
+	two players
+	take turns placing one piece at a time
+	there must be some way to evaluate board utility
 */
 
 
 
 var games = {
-    "tic-tac-toe": {
-        dims: [3, 3],
-        validMoves: {
-            "X": "X",
-            "O": "O"
-        },
-        //utility goal is the amount of utility each player wants. In this case, X wants a lot of negative utility, and O wants a lot of positive utility.
-        utilityGoals: {
-            "X": -1e101,
-            "O": 1e101
-        },
+	"5-in-a-row": {
+		//returns a list of name coordinates associated with board states
+		//ex: ["23", "...XO..X..O...XX..O..O"]
+		constructChildren: (boardState, player) => {
+			var sz = games["5-in-a-row"].dims;
+			var arr = [];
+			for (var y=0; y<sz[1]; y++) {
+				for (var x=0; x<sz[0]; x++) {
+					if (boardState[(y * sz[1]) + x] == ".") {
+						arr[x + (sz[1] * y)] = [boardChars[x] + boardChars[y], placePiece(boardState, x + (sz[1] * y), player)];
+					}
+				}
+			}
+			return arr;
+		},
+		dims: [10, 10],
+		players: ["X", "O"],
+		thinkDist: 4,
+		//utility goal is the amount of utility each player wants. In this case, X wants a lot of negative utility, and O wants a lot of positive utility.
+		utilityGoals: {
+			"X": -1,
+			"O": 1
+		},
 
-        isDone: (boardState) => {
-            return (games["tic-tac-toe"].utility(boardState) != 0);
-        },
+		utility: (boardState) => {
+			return utilityForNxMStandard(boardState, 5, games["5-in-a-row"].dims[0], games["5-in-a-row"].dims[1])
+		}
+	},
 
-        //x counts as negative utility
-        utility: (boardState) => {
-            return utilityForNxNStandard(boardState, 3);
-        }
-    },
-    "order-and-chaos": {
-        boardDimensions: [6, 6],
-        validMoves: {
-            "order": "XO",
-            "chaos": "XO"
-        }
-    }
+	"connect-4": {
+		constructChildren: (boardState, player) => {
+			//one command for each row
+			var sz = games["connect-4"].dims;
+			var buffer1 = [];
+
+			for (var x=0; x<sz[0]; x++) {
+				//is the row open?
+				if (boardState[x] == ".") {
+					//move down util found one
+					for (var y=1; y<sz[1]; y++) {
+						if (boardState[(sz[0] * y) + x] != ".") {
+							buffer1[x] = [boardChars[x], placePiece(boardState, (sz[0] * (y - 1)) + x, player)];
+							y = sz[1] + 2;
+						}
+					}
+
+					//if out of zone, set to last square
+					if (y < sz[1] + 1) {
+						buffer1[x] = [boardChars[x], placePiece(boardState, (sz[0] * (sz[1] - 1)) + x, player)];
+					}
+				}
+			}
+			return buffer1;
+		},
+		dims: [7, 6],
+		players: ["X", "O"],
+		thinkDist: 6,
+		validMoves: {
+			"X": "X",
+			"O": "O"
+		},
+		utilityGoals: {
+			"X": -1,
+			"O": 1
+		},
+
+		utility: (boardState) => {
+			return utilityForNxMStandard(boardState, 4, games["connect-4"].dims[0], games["connect-4"].dims[1]);
+		}
+	},
+
+	"tic-tac-toe": {
+		constructChildren: (boardState, player) => {
+			var buffer1 = [];
+
+			//add all available squares
+			for (var c=0; c<boardState.length; c++) {
+				if (boardState[c] == ".") {
+					buffer1[c] = [boardChars[c], placePiece(boardState, c, player)];
+				}
+			}
+
+			return buffer1;
+		},
+		dims: [3, 3],
+		players: ["X", "O"],
+		thinkDist: 10,
+		validMoves: {
+			"X": "X",
+			"O": "O"
+		},
+		utilityGoals: {
+			"X": -1,
+			"O": 1
+		},
+
+		utility: (boardState) => {
+			return utilityForNxMStandard(boardState, 3, games["tic-tac-toe"].dims[0], games["tic-tac-toe"].dims[1]);
+		}
+	},
+	
+	//FIX CONSTRUCT CHILDREN HERE
+	"order-and-chaos": {
+		constructChildren: (boardState, player) => {
+			var buffer1 = [];
+
+			//add all available squares
+			for (var c=0; c<boardState.length; c++) {
+				if (boardState[c] == ".") {
+					buffer1[c] = [boardChars[c], c];
+				}
+			}
+			return buffer1;
+		},
+		dims: [6, 6],
+		players: ["order", "chaos"],
+		thinkDist: 5,
+		validMoves: {
+			"order": "XO",
+			"chaos": "XO"
+		},
+		utilityGoals: {
+			"order": 1,
+			"chaos": 0,
+		},
+		utility: (boardState) => {
+			//divide into 4 boards, those'll act as mini 5x5s.
+			var minis = [];
+			
+			//if either x or o wins, order wins (so take the absolute value of a typical utility function)
+		}
+	}
+}
+
+
+var games_desc = {
+	"tic-tac-toe": [`Played with two players on a 3 by 3 board.`, `Each player tries to make 3 in a row to win the game.`, `One player plays only Xs, and one player plays only Os.`, `X goes first.`],
+	"5-in-a-row": [`Similar to tic-tac-toe, but 5 in a row`, `is required to win, and the board is 10 by 10.`],
+	"order-and-chaos": [`Played on a 6 by 6 board.`, `One player is order, and the other is chaos.`, `Both players can place an X or O.`, `Order wants to make at least 5 in a row.`, `Chaos wants to prevent that.`, `Order plays first.`],
 }
--- X-O-games/main.js
+++ X-O-games/main.js
@@ -2,21 +2,29 @@
 
 
 window.onload = setup;
+var asyncBuffer;
 
-var game_board;
+var boardChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-=,.;'[]~!@#$%^&*()_+";
+var board;
+
+var game_auto = false;
+var game_computerIs = undefined;
 var game_dat;
-var game
+var game_maxViewDepth = 12;
+var game_toPlay;
 
 var textDelayCurrent = 0;
 var textDelay = 2;
 var textLine = 0;
-
 var textBuffer = [];
-var textCommands = [[`quit`, () => {state = 0; text_setStandard();}]];
+var textCommands = [];
 
-var state = 0;
+function setStandardTextCommands() {
+	textCommands = [[`quit`, () => {text_setStandard(); stopAsyncActivity();}]];
+}
 
 function setup() {
+	setStandardTextCommands();
 	text_setStandard();
 	document.getElementById("terminal-input").addEventListener("keyup", acceptTextCommand, false);
 }
@@ -31,57 +39,13 @@ function acceptTextCommand(a) {
 
 	//detect commands and clear box if accepted
 	for (var e=0; e<textCommands.length; e++) {
-		if (command == textCommands[e][0]) {
+		if (command.toUpperCase() == textCommands[e][0].toUpperCase()) {
 			textCommands[e][1]();
 			commandLocation.value = "";
+			return;
 		}
 	}
 }
-
-function text_setPreparePlayers(gameKey) {
-	//commands
-	textCommands = textCommands.slice(0, 1);
-	textCommands.push([players[0], () => {}]);
-
-	//text
-	textBuffer = [`Enter number of human players.`, `[2] or [1]`];
-}
-
-
-function text_setPrepareRoles(gameKey) {
-	var players = Object.keys(games[gameKey].validMoves);
-
-	//commands
-	textCommands = textCommands.slice(0, 1);
-	textCommands.push([players[0], () => {}])
-
-	game_dat = games[gameKey];
-	board = ".".repeat(game_dat.dims[0] * game_dat.dims[1]);
-
-	textBuffer = [`Would you like to be [${players[0]}] or [${players[1]}]?`];
-	displayText();
-}
-
-function text_setStandard() {
-	textBuffer = [
-		`Welcome.`,
-		`Typable commands will be in [square brackets].`,
-		`At any time, you may [quit] to this state.`,
-		`Please type a game selection:`,
-	];
-	
-	//set text + commands
-	textCommands = textCommands.slice(0, 1);
-	Object.keys(games).forEach(g => {
-		textBuffer.push(`[${g}]`);
-		textCommands.push([g, () => {text_setPrepare(g); state = 1;}])
-	});
-	displayText();
-
-	//set commands
-}
-
-
 
 function displayText() {
 	textLine = 0;
@@ -114,54 +78,342 @@ function displayTextRecursive() {
 }
 
 
+function generateMoveTree(boardState, maxDepth, player) {
+	var selfNode = {
+		board: boardState,
+		children: [],
+		utility: game_dat.utility(boardState),
+	};
+
+	//if the utility isn't undefined, it's an end node, and it can be returned
+	//alternatively, it's being cut off
+	if (selfNode.utility != undefined) {
+		return selfNode;
+	}
+
+	if (maxDepth <= 0) {
+		selfNode.utility = 0;
+		return selfNode;
+	}
+
+	//if the utility is undefined, gotta recurse
+	var minUtilDist = 1e1001;
+	var newNode;
+	//figure out children
+	var children = game_dat.constructChildren(selfNode.board, player);
+
+	for (var c=0; c<children.length; c++) {
+		if (children[c] != undefined) {
+			newNode = generateMoveTree(children[c][1], maxDepth - 1, incrementPlayer(player));
+			selfNode.children.push(newNode);
+
+			//if that possible utility is closer to self's goal, then make that the utility for the boare
+			if (Math.abs(newNode.utility - game_dat.utilityGoals[player]) < minUtilDist) {
+				minUtilDist = Math.abs(newNode.utility - game_dat.utilityGoals[player]);
+				selfNode.utility = newNode.utility;
+
+				//if it's the closest possible, it's not worth continuing (can't find a closer state)
+				// if (minUtilDist == 0) {
+				// 	return selfNode;
+				// }
+			}
+		}
+		
+	}
+
+	return selfNode;
+}
+
+function incrementPlayer(player) {
+	//walk through the players
+	return game_dat["players"][(game_dat["players"].indexOf(player) + 1) % game_dat["players"].length];
+}
+
+function placePiece(boardState, location, piece) {
+	return boardState.slice(0, location) + piece + boardState.slice(location + 1);
+}
+
+function stopAsyncActivity() {
+	window.clearTimeout(asyncBuffer);
+	game_auto = false;
+}
+
+
+function text_runGame() {
+	game_toPlay = incrementPlayer(game_toPlay);
+	setStandardTextCommands();
+
+	//figure out if it's a computer or human turn
+	var turnIsRobot = game_auto || (game_toPlay == game_computerIs);
+
+	//first display current board state. I put breaks in so this is all done in one pass, no matter the draw speed.
+	var boardStr = "<br>";
+	var commandStr = "";
+	//I put the commands ina list so javascript arguments don't freak out on me
+	var commandList = game_dat.constructChildren(board, game_toPlay);
+	var commandLen = 0;
+	for (var a=0; a<commandList.length; a++) {
+		if (commandList[a] != undefined) {
+			commandLen = commandList[a][0].length;
+			a = commandList.length + 1;
+		}
+	}
+	for (var q=0; q<game_dat.dims[0] * game_dat.dims[1]; q+=game_dat.dims[0]) {
+		if (!turnIsRobot) {
+			for (var y=0; y<game_dat.dims[0]; y++) {
+				if (commandList[q+y] == undefined) {
+					commandStr += ` `.repeat(2 + commandLen);
+				} else {
+					commandStr += `[${commandList[q+y][0]}]`;
+				}
+			}
+		}
+		
+		boardStr += `${board.slice(q, q+game_dat.dims[0])}      ${commandStr}<br>`;
+		commandStr = "";
+	}
+	
+
+
+	textBuffer = [boardStr];
+
+	//is the game over? 
+	var nowUtility = game_dat.utility(board);
+	if (nowUtility != undefined) {
+		textBuffer.push("");
+		if (nowUtility == game_dat.utilityGoals[game_dat.players[0]]) {
+			textBuffer.push(`${game_dat.players[0]} has won.`);
+		} else if (nowUtility == game_dat.utilityGoals[game_dat.players[1]]) {
+			textBuffer.push(`${game_dat.players[1]} has won.`);
+		} else {
+			textBuffer.push(`Stalemate.`);
+		}
+		
+		if (game_auto) {
+			asyncBuffer = window.setTimeout(() => {
+				text_setPreparePlayers();
+				text_runGame();
+			}, 100);
+		} else {
+			textBuffer.push(`You may [quit] or [restart].`);
+			textCommands.push(['restart', () => {text_setPreparePlayers();}]);
+		}
+		
+
+		displayText();
+		return;
+	}
+	
+	if (!game_auto) {
+		textBuffer.push(`<br>${game_toPlay} to move.`);
+	}
+
+	//commands
+	if (turnIsRobot) {
+		//think hard
+		var moveTree = generateMoveTree(board, game_maxViewDepth, game_toPlay);
+		var moveUtilDist = undefined;
+		var moveBests = [];
+		var classifyText = "";
+		//choose move based on utility of children
+		for (var k=0; k<moveTree.children.length; k++) {
+			classifyText = (moveTree.children[k].utility == game_dat.utilityGoals[game_toPlay]) ? `win` : (moveTree.children[k].utility == 0) ? `draw` : `loss`;
+
+			if (moveUtilDist == undefined || Math.abs(moveTree.children[k].utility - game_dat.utilityGoals[game_toPlay]) < moveUtilDist) {
+				moveUtilDist = Math.abs(moveTree.children[k].utility - game_dat.utilityGoals[game_toPlay]);
+				moveBests = [moveTree.children[k].board];
+			} else if (Math.abs(moveTree.children[k].utility - game_dat.utilityGoals[game_toPlay]) == moveUtilDist) {
+				//if it's equal, add it to the possible moves
+				moveBests.push(moveTree.children[k].board);
+			}
+		}
+
+		//choose best move
+		if (moveBests.length == 0) {
+			console.log(`something's gone wrong!!!`);
+		}
+		if (moveBests.length < 2) {
+			board = moveBests[0];
+			//console.log(`forced`, board);
+		} else {
+			board = moveBests[Math.floor(Math.random() * (moveBests.length-0.01))];
+		}
+		asyncBuffer = window.setTimeout(() => {
+			text_runGame();
+		}, 1 + (20 * game_auto));
+	} else {
+		//allow input from player
+		commandList.forEach(c => {
+			textCommands.push([c[0], () => {board = c[1]; text_runGame();}])
+		});
+	}
+
+	//if it's a draw, display that.
+	
+
+
+	displayText();
+}
+
+function text_setCMDListing() {
+	//drawing
+	textBuffer = [
+		`COMMAND STRUCTURE: [command name] [arg1] [arg2] [arg3]`,
+		`Typed as: command name arg1 arg2 arg3`,
+		`[quit] - At any time, quit to the main space.`,
+		//`[rules] [game] - View the rules of a game.`,
+	];
+
+	//commands
+	setStandardTextCommands();
+	textCommands = [...textCommands,
+		[],
+	]
+
+	displayText();
+}
+
+
+function text_setPreparePlayers(gameKey) {
+	if (gameKey != undefined) {
+		game_dat = games[gameKey];
+	}
+	game_maxViewDepth = game_dat.thinkDist;
+	game_toPlay = game_dat.players[1];
+	board = ".".repeat(game_dat.dims[0] * game_dat.dims[1]);
+
+	//commands
+	setStandardTextCommands();
+	textCommands.push(['2', () => {game_computerIs = ""; text_runGame();}]);
+	textCommands.push(['1', () => {text_setPrepareRoles();}]);
+	textCommands.push(['0', () => {game_auto = true; text_runGame();}]);
+
+	//text
+	textBuffer = [`Enter number of human players.`, `[2] or [1]`];
+
+	displayText();
+}
+
+
+function text_setPrepareRoles() {
+	var players = game_dat.players;
+	//commands
+	setStandardTextCommands();
+	textCommands.push([players[0], () => {game_computerIs = players[1]; text_runGame();}]);
+	textCommands.push([players[1], () => {game_computerIs = players[0]; text_runGame();}]);
+
+	textBuffer = [`Would you like to be [${players[0]}] or [${players[1]}]?`];
+	displayText();
+}
+
+function text_setStandard() {
+	textBuffer = [
+		`Welcome.`,
+		`Typable commands will be in [square brackets].`,
+		`You may request a full [list] of commands.`,
+		`Computer opposition is not yet fully complete. Please be prepared`,
+		`for imperfect performance if playing with <2 players.`,
+		``,
+		`Please type a game selection:`,
+	];
+	
+	//set text + commands
+	setStandardTextCommands();
+	textCommands.push(["list", () => {text_setCMDListing();}]);
+	Object.keys(games).forEach(g => {
+		textBuffer.push(`[${g}]`);
+		textCommands.push([g, () => {text_setPreparePlayers(g);}])
+	});
+
+	displayText();
+}
+
+
+
 //returns the standard utility for an n x n board (X is negative, O is positive, tic-tac-toe rules but on an n by n board)
-function utilityForNxNStandard(boardState, n) {
+function utilityForNxMStandard(boardState, requiredInARow, n, m) {
 	var valuesFromBoard = [];
+
+	var inARow = 0;
+	var rowVal;
+	//valuos
 	for (var c=0; c<boardState.length; c++) {
 		valuesFromBoard[c] = +(boardState[c] == "O") - +(boardState[c] == "X")
 	}
-	//keep track of the value for every row, column, and diagonal.
-	var utilStore = 0;
 
-	//diagonal1
-	for (var a=0; a<n*n; a+=n+1) {
-		utilStore += valuesFromBoard[a];
+	//DR diagonals
+	for (s=0; s<n-requiredInARow+1; s++) {
+		//s is the horizontal offset, a is the diagonal offset
+		for (var a=0; a<Math.min(n-s, m); a++) {
+			if (valuesFromBoard[s + a + (a * n)] != 0 && valuesFromBoard[s + a + (a * n)] == rowVal) {
+				inARow += 1;
+				if (inARow >= requiredInARow) {
+					return rowVal;
+				}
+			} else {
+				inARow = 1;
+				rowVal = valuesFromBoard[s + a + (a * n)];
+			}
+		}
+		rowVal = undefined;
 	}
-	if (Math.trunc(utilStore / n) != 0) {
-		return utilStore / n;
+
+	for (s=1; s<m+1-requiredInARow; s++) {
+
 	}
-	utilStore = 0;
 	
 
-	//diagonal2
-	for (a=n-1; a<n*n-1; a+=n-1) {
-		utilStore += valuesFromBoard[a];
-	}
-	if (Math.trunc(utilStore / n) != 0) {
-		return utilStore / n;
-	}
-	utilStore = 0;
+	//DL diagonals
+	for (var s=requiredInARow-1; s<n; s++) {
+		for (var a=0; a<Math.min(s+1, m); a++) {
+			if (valuesFromBoard[s - a + (a * n)] != 0 && valuesFromBoard[s - a + (a * n)] == rowVal) {
+				inARow += 1;
+				if (inARow >= requiredInARow) {
+					return rowVal;
+				}
+			} else {
+				inARow = 1;
+				rowVal = valuesFromBoard[s - a + (a * n)];
+			}
+		}
 
+		rowVal = undefined;
+	}
 
 	//rows
-	for (c=0; c<n; c++) {
-		for (a=0; a<n; a++) {
-			utilStore += valuesFromBoard[(n * c) + a];
+	for (var c=0; c<m; c++) {
+		for (var a=0; a<n; a++) {
+			if (valuesFromBoard[(n * c) + a] != 0 && valuesFromBoard[(n * c) + a] == rowVal) {
+				inARow += 1;
+				if (inARow >= requiredInARow) {
+					return rowVal;
+				}
+			} else {
+				inARow = 1;
+				rowVal = valuesFromBoard[(n * c) + a];
+			}
 		}
-		if (Math.trunc(utilStore / n) != 0) {
-			return utilStore / n;
-		}
-		utilStore = 0;
+		rowVal = undefined;
 	}
 
 	//columns
-	for (c=0; c<n; c++) {
+	for (c=0; c<m; c++) {
 		for (a=0; a<n; a++) {
-			utilStore += valuesFromBoard[(a * n) + c];
+			if (valuesFromBoard[(a * n) + c] != 0 && valuesFromBoard[(a * n) + c] == rowVal) {
+				inARow += 1;
+				if (inARow >= requiredInARow) {
+					return rowVal;
+				}
+			} else {
+				inARow = 1;
+				rowVal = valuesFromBoard[(a * n) + c];
+			}
 		}
-		if (Math.trunc(utilStore / n) != 0) {
-			return utilStore / n;
-		}
-		utilStore = 0;
+		rowVal = undefined;
+	}
+
+	//if there are no moves left to play, it's a draw
+	if (boardState.indexOf(".") == -1) {
+		return 0;
 	}
 }
