508f0c9f23d1a10cc87c9bf9b1789f31c06d97d0
BobbyClementine <davis@zetlan.net>
2022-01-03 16:05:42 -0500

    Added file

---
+++ working-title/functions-drawing.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {Number} x x to put the first key at
+ * @param {Number} y y to put the first key at
+ * @param {Number} keySize the size of each key
+ * @param {Array} keyArr the array of keys to draw
+ * @param {Array} matrix the coordinates to offset keys by
+ * @param {Boolean} drawKeycaps whether to draw letters or not
+ */
+function drawKeyboard(x, y, keySize, keyArr, matrix, drawKeycaps) {
+	
+}
