8a05db41325f826bfdb226a7afb9b4e0cb147a8b
BobbyClementine <davis@zetlan.net>
2022-01-14 08:26:34 -0500

    added canvas transform to preferences

--- working-title/functions-drawing.js
+++ working-title/functions-drawing.js
@@ -6,6 +6,8 @@ function setCanvasPreferences() {
 	canvas.width = size * 0.9;
 	canvas.height = size / ratio * 0.9;
 
+	ctx.setTransform(1, 0, 0, -1, 0, 0);
+	ctx.translate(0, -canvas.height);
 	ctx.textBaseline = "middle";
 	ctx.imageSmoothingEnabled = false;
 	ctx.lineWidth = Math.ceil(canvas.height / 240);
