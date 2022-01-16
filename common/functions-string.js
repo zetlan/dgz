c833f4eadf4bf6074b2701c5abd04453f460da3e
BobbyClementine <davis@zetlan.net>
2022-01-15 20:39:40 -0500

    Misc. dev.

--- common/functions-string.js
+++ common/functions-string.js
@@ -1,11 +1,28 @@
 
+//This file is a little different from my other function files. Instead of creating entirely separate functions, these add themselves to the string class.
+//The goal with this was to make it easier to edit strings.
+
+
 //takes a string and cuts it into equal-sized pieces with a specified length.
 //If there is a remainder, it will be added to the end.
-function chop(string, length) {
-	length = length ?? string.length;
-	var bits = [];
-	for (var d=0; d<string.length; d+=length) {
-		bits.push(string.slice(d, d+length));
+if (!String.prototype.chop) {
+	String.prototype.chop = function (size) {
+		size = size ?? this.length;
+		var bits = [];
+		for (var d=0; d<this.length; d+=size) {
+			bits.push(this.slice(d, d+size));
+		}
+		return bits;
 	}
-	return bits;
+} else {
+	console.error(`Chop conflicts with an existing function!`);
+}
+
+//returns a string with characters at a certain spot in it replaced
+if (!String.prototype.replaceAt) {
+	String.prototype.replaceAt = function (index, newChars) {
+		return this.slice(0, index) + newChars + this.slice(index+1);
+	}
+} else {
+	console.error(`ReplaceAt conflicts with an existing function!`);
 }
