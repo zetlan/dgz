551cc6372f16376f2ac4601c0090c2fd6aa7cf30
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2022-02-05 15:31:47 -0500

    Misc. changes

--- simon-says/main.js
+++ simon-says/main.js
@@ -16,6 +16,9 @@ var board_tileTol = 0.55;
 var board_limits = [2, 6];
 var board_height = 0.45;
 
+var canvas;
+var ctx;
+
 var clock_pos = 0.93;
 
 var color_arrows = "#88F";
@@ -26,7 +29,7 @@ var color_cursor = "#FFF";
 var color_cursor2 = "#F00";
 var color_cursor3 = "#606";
 var color_board = "#114";
-var color_menuBg = "#AAB";
+var color_menuBg = "#CCF";
 var color_select = "#66A";
 var color_selectRobot = "#00F";
 var color_selectWrong = "#800";
@@ -35,6 +38,8 @@ var color_timerArmActive = "#7F0";
 
 var cursor_x;
 var cursor_y;
+
+var display_sizes = [240, 480, 720, 1080];
 
 var game_active = false;
 var game_forceLose = false;
@@ -54,8 +59,10 @@ var light_tile = [];
 
 var menu_active = false;
 var menu_darkenAmount = 0.2;
-var menu_height = 0.1;
-var menu_iconHeight = 0.08;
+var menu_height = 0.15;
+var menu_iconHeight = 0.1;
+var menu_items = ["speed", "rounds", "sound", "timer", "dispSize"];
+var menu_itemsAvailable = [true, false, true, false, true];
 var menubar_xPos = 0.95;
 
 var rounds_index = 1;
@@ -73,12 +80,7 @@ var timer_count = 0;
 function setup() {
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
-	ctx.lineWidth = 4;
-	ctx.lineJoin = "round";
-	ctx.font = "20px Ubuntu";
-	ctx.textAlign = "center";
-
-	board_screenSize *= canvas.height;
+	setCanvasPreferences();
 
 	cursor_x = canvas.width / 2;
 	cursor_y = canvas.height / 2;
@@ -91,22 +93,22 @@ function update() {
 	ctx.fillStyle = color_bg;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	if (menu_active) {
-		return;
-	}
-
 	//timing logic
-	if (game_active) {
+	if (game_active && !menu_active) {
 		runGame();
 	}
-	
 
+	//board + text
+	drawArrows();
 	drawBoard();
-	drawMenu();
-
-	//text
 	ctx.fillStyle = color_text;
 	ctx.fillText(game_text, canvas.width * 0.5, canvas.height * 0.95);
+
+	//menu
+	drawMenuItem(canvas.width * 0.925, canvas.height * menu_iconHeight * 0.7, canvas.height * menu_iconHeight / 3, "settingGear");
+	if (menu_active) {
+		drawMenu();
+	}
 
 	drawCursor();
 
@@ -150,11 +152,6 @@ function runGame() {
 			}
 		}
 	}
-}
-
-//returns true if cursor is close enough to a point
-function getMenubarClickable(height) {
-	return (getDistance([cursor_x, cursor_y], [canvas.width * menubar_xPos, height]) < menubar_radius * 1.2);
 }
 
 function startGame() {
@@ -206,7 +203,7 @@ function handleKeyPress(a) {
 			menu_active = false;
 			break;
 		case "Space":
-			if (!game_active) {
+			if (!game_active && !menu_active) {
 				startGame();
 			}
 			a.preventDefault();
@@ -231,17 +228,20 @@ function handleMouseMove(a) {
 
 function handleMouseDown(a) {
 	//updateMenubar returns true if an item is changed, if not do other things
+
 	var handled = updateMenubar(cursor_x, cursor_y);
 
 	//registering input in game if it's the person's turn
 	if (game_active && !handled && game_humanTurn && (game_move < game_path.length)) {
 		//making sure click is in board bounds
-		var inBoundsX = Math.abs((canvas.width / 2) - cursor_x) < (board_screenSize / 2);
-		var inBoundsY = Math.abs((canvas.height * board_height) - cursor_y) < (board_screenSize * 0.48);
+		var trueSize = board_screenSize * canvas.height;
+		var inBoundsX = Math.abs((canvas.width / 2) - cursor_x) < trueSize * 0.5;
+		var inBoundsY = Math.abs((canvas.height * board_height) - cursor_y) < trueSize * 0.48;
+		
 		if (inBoundsX && inBoundsY) {
 			light_time = light_timeMax;
-			light_tile = [Math.round((cursor_x - ((canvas.width / 2) - (board_screenSize / 2) + (board_screenSize / (board_rowNum * 2)))) / (board_screenSize / board_rowNum)), 
-						Math.round((cursor_y - ((canvas.width * (board_height - 0.1)) - (board_screenSize / 2) + (board_screenSize / (board_rowNum * 2)))) / (board_screenSize / board_rowNum))];
+			light_tile = [Math.round((cursor_x - ((canvas.width / 2) - (trueSize / 2) + (trueSize / (board_rowNum * 2)))) / (trueSize / board_rowNum)), 
+						Math.round((cursor_y - ((canvas.width * (board_height - 0.1)) - (trueSize / 2) + (trueSize / (board_rowNum * 2)))) / (trueSize / board_rowNum))];
 
 			//validating press is correct
 			if (light_tile[0] == game_path[game_move][0] && light_tile[1] == game_path[game_move][1]) {
@@ -264,53 +264,47 @@ function handleMouseDown(a) {
 }
 
 //detecting clicks on the menubar
-function updateMenubar(clickX, clickY) {
-	//I know if/else is probably not the best way to do it, but I don't know if I can turn this into a switch statement
-	//also it doesn't matter that much since they're clearly seperated into boxes
-	//I don't want to complicate things by making classes
+function updateMenubar() {
+	//clicking the settings gear
+	if (!menu_active) {
+		//clicking the settings gear
+		if (getDistance([cursor_x, cursor_y], [canvas.width * 0.925, canvas.height * menu_iconHeight * 0.7]) < canvas.height * menu_iconHeight / 2) {
+			menu_active = true;
+			return true;
+		}
 
+		//clicking the left / right board arrows
+		if (Math.abs(cursor_y - (canvas.height * 0.5)) < canvas.height * 0.1 && ((cursor_x < canvas.width * 0.1 && board_rowNum > board_limits[0]) || (cursor_x > canvas.width * 0.9 && board_rowNum < board_limits[1]))) {
+			//increasing vs decreasing
 	
-	//board speed
-	if (getMenubarClickable(canvas.height * clock_pos)) {
-		light_speed += 1;
-		if (light_speed > game_speeds.length - 1) {
-			light_speed = 0;
+			board_rowNum = clamp(board_rowNum + boolToSigned(cursor_x > canvas.width * 0.5), board_limits[0], board_limits[1]);
+	
+			stopGame();
+			game_text = `No game active, press space to start a ${board_rowNum}x${board_rowNum} game`;
 		}
-
-		light_timeMax = game_speeds[light_speed];
+		return false;
 	}
 
-	//number of rounds (requires no game active)
-	else if (!game_active && getMenubarClickable(canvas.height * rounds_pos)) {
-		rounds_index += 1;
-		if (rounds_index > game_rounds.length - 1) {
-			rounds_index = 0;
+	//if already in the menu, and clicking outside of it, go outside the menu
+	var xDist = Math.abs(cursor_x - canvas.width / 2);
+	var yDist = Math.abs(cursor_y - canvas.height / 2);
+	if (xDist > canvas.width * menu_iconHeight * menu_items.length * 0.5 || yDist > canvas.height * menu_height * 0.5) {
+		menu_active = false;
+		return true;
+	}
+
+	//interact with the menu elements
+	var targetX;
+	var targetY;
+	for (var i=0; i<menu_items.length; i++) {
+		targetX = canvas.width * (0.5 + 0.95 * menu_iconHeight * (i + 0.5 - menu_items.length * 0.5));
+		targetY = canvas.height * 0.5;
+
+		if (getDistance([cursor_x, cursor_y], [targetX, targetY]) < canvas.height * menu_iconHeight * 0.5) {
+			//the item is interactable
+			interactMenuItem(menu_items[i]);
+			return true;
 		}
-		rounds_max = game_rounds[rounds_index];
 	}
-
-	//sound
-	else if (getMenubarClickable(canvas.height * sound_pos)) {
-		//alternate between 0, 0.5, and 1
-		sound_on = (sound_on + 0.5) % 1.5
-
-		audio_happy.volume = sound_on;
-		audio_neutral.volume = sound_on;
-		audio_negative.volume = sound_on;
-	}
-
-	//timer active
-	else if (getMenubarClickable(canvas.height * timer_pos)) {
-		timer_active = !timer_active;
-	}
-
-	//board size
-	else if ((cursor_x < canvas.width * 0.1 && board_rowNum > board_limits[0]) || (cursor_x > canvas.width * 0.9 && board_rowNum < board_limits[1])) {
-		//increasing vs decreasing
-
-		board_rowNum = clamp(board_rowNum + boolToSigned(cursor_x > canvas.width * 0.5), board_limits[0], board_limits[1]);
-
-		stopGame();
-		game_text = `No game active, press space to start a ${board_rowNum}x${board_rowNum} game`;
-	}
+	return true;
 }
