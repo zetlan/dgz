a05c7f4bbe754ff0072e2df4401f9be041401783
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2022-03-01 14:40:42 -0500

    Finished

--- AI-work/regex/regexUpload1.py
+++ AI-work/regex/regexUpload1.py
@@ -16,19 +16,48 @@ matchers = [
     #for the record, this line is supposed to match social security numbers. ddd-dd-dddd where there can be any number of spaces between the digits and either 0 or 1 dashes between them.
     r"/^\d{3}\s*-?\s*\d\d\s*-?\s*\d{4}$/",
     r"/^.*?[dD].*?\b/m",
-    r"/^0$|^1$|^0[01]*0$|^1[01]*1$|^$/"
+    r"/^0$|^1$|^0[01]*0$|^1[01]*1$|^$/",
 
 
     #40-49
-    r"/^[xX.Oo]{64}$/",
-    r"/[xXOo]*\.[xXOo]*/",
-    r"/^\.|\.$|^x+o*\.|\.o*x+$/",
-    r"/^(..)*.$/",
-    r"/^0([01]{2})*$|^1[01]([01]{2})*$/",
+    #regex is most purely enjoyed when you don't see anything except the characters. For this reason, there will be no future comments about the regex, apart from line numbers (I'm not a masochist)
+    #for the optimal viewing experience, it is recommended that you use an IDE which does not use syntax highlighting, or alternatively just open it in notepad. 
+    #Black + white shapes, after all, are what regex is all about.
+    r"/^[x.o]{64}$/i",
+    r"/^[xo]*\.[xo]*$/i",
+    r"/^(x+o*)?\.|\.(o*x+)?$/i",
+    r"/^([^!]{2})*[^!]$/",
+    r"/^(0|1[01])([01]{2})*$/",
+    r"/\w*(a[eiou]|e[aiou]|i[aeou]|o[aeiu]|u[aeio])\w*/i",
+    r"/^((10)*0*)*1*$|^0$/",
+    r"/^[bc]*[abc][bc]*$/",
+    r"/^(a[bc]*a|[bc]+?)(([bc]*?a){2})*[bc]*$/",
+    r"/^(2|1[02]*1)(([02]*?1){2})*[02]*$/",
+
+
+    #50-59
+    r"/\w*(\w)\w*?\1\w*/i",
+    r"/\w*(\w)(\w*\1){3}\w*/i",
+    r"/^([01])([01]*\1)?$/",
+    r"/(?=\w{6}\b)\b\w*cat\w*\b/i",
+    r"/(?=\w*?bri)(?=\w*?ing)\b\w{5,9}\b/i",
+    r"/\b(?!\w*cat)\w{6}\b/i",
+    r"/\b(?:([a-z])(?!\w*\1))+\b/i",
+    r"/(?!.*10011)^[01]*$/",
+    r"/\b\w*?([aeiou])(?!\1)[aeiou]\w*?\b/i",
+    r"/(?!.*1[01]1)^[01]*$/",
+
+
+    #60-69
+    r"/(?!.*010)^[01]*$/",
+    r"/(?!.*(010|101))^[01]*$/",
+    r"/^([01])([01]*\1)?$/",
+    r"/\b(?!\w*(\w)\w*\1\b)\w+/i",
+    r"/\b(?=\w*(\w)\1\w*(\w)\2\w*)\w+\b/",
     r"//",
-    r"//",
-    r"/^[bc]*a?[bc]*$/",
-    r"/^((([bc]*a[bc]*){2})*)$/",
+    r"/\b(?=\b[^a ]*[a][^a ]*\b)(?=\b[^e ]*[e][^e ]*\b)(?=\b[^i ]*[i][^i ]*\b)(?=\b[^o ]*[o][^o ]*\b)(?=\b[^u ]*[u][^u ]*\b)\w*\b/i",
+    r"/(?=((1*0){2})*1*01*$)(?=((0*1){2})*0*$)^[01]*$/",
+    r"/^0$|^(?=0*1)(0|1(01*0)*1)+$/",
     r"//"
 ]
 
