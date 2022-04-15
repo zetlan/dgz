2d663a15ef991e0e0b0f29ec76f55dd33a713da4
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2022-04-15 12:20:34 -0400

    Misc

--- tetris/main.js
+++ tetris/main.js
@@ -5,10 +5,21 @@ var animation;
 var canvas;
 var ctx;
 
-var ai_name = "block-1";
-var ai_population = [];
 var ai_best;
 var ai_bestFitness;
+var ai_endStr = `GAME OVER`;
+var ai_generation = 0;
+var ai_name = "block-1";
+var ai_mutationRate = 0.2;
+var ai_mutationAmount = 0.05;
+var ai_population = [];
+var ai_populationPaired = [];
+var ai_populationGoal = 600;
+//how many points the AI gets for clearing x number of lines at the same time
+var ai_scoring = [0, 40, 100, 300, 1200];
+var ai_sharesMax = 150;
+var ai_sharesRate = 0.85;
+var ai_trainGames = 3;
 
 
 var audio_channel1;
@@ -21,25 +32,21 @@ var board_clearTime = 15;
 var board_screenPercentage = 0.9;
 var board_width = 10;
 var board_height = 20;
+var board_heightBuffer = 2;
 var board_verticalAdjust = 0.7;
 var board_linesRequired = 10;
 var boards = [];
+
+var color_bg_menu = "#A57548";
+var color_text = "#BDFFF9";
+var color_gameEndBg = "#2e103a";
 
 var framesPerSecond = 60;
 var frameTime = 1 / framesPerSecond;
 var frameIncrement = 60;
 var frameBuffer = [];
 
-
-
-
-
-var color_bg_menu = "#A57548";
-var color_text = "#BDFFF9";
-var color_gameEndBg = "#B5FFE0";
-
-
-
+var fileObj;
 
 var game_levels = [];
 var game_endOpacity = 0;
@@ -65,21 +72,23 @@ var menu_settingMarginW = 0.1;
 
 var sprint_lines = 40;
 
+var scores_max = 10;
+
 let state_functions_main = [
-	menu_execute,
-	endless_execute,
-	sprint_execute,
-	competition_execute,
-	aiTrain_execute,
-	aiPlay_execute,
+	execute_menu,
+	execute_endless,
+	execute_sprint,
+	execute_competition,
+	execute_aiTrain,
+	execute_aiPlay,
 ]
 let state_functions_keyPress = [
-	menu_handleKeyPress,
-	game_handleKeyPress,
-	game_handleKeyPress,
-	competition_handleKeyPress,
-	aiTrain_handleKeyPress,
-	aiPlay_handleKeyPress,
+	handleKeyPress_menu,
+	handleKeyPress_game,
+	handleKeyPress_game,
+	handleKeyPress_competition,
+	handleKeyPress_aiTrain,
+	handleKeyPress_aiPlay,
 ]
 
 function handleInput(a) {
@@ -102,7 +111,7 @@ function main() {
 	animation = window.requestAnimationFrame(main);
 }
 
-function aiTrain_execute() {
+function execute_aiTrain() {
 	//audio + background
 	audio_channel1.target = undefined;
 	audio_channel1.tick();
@@ -110,57 +119,20 @@ function aiTrain_execute() {
 	ctx.fillStyle = color_palettes[0].bg;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	//top text
-	ctx.font = `${canvas.height / 20}px Ubuntu`;
-	ctx.fillStyle = color_text;
-	ctx.textAlign = "center";
-	ctx.fillText(`AI Interface`, canvas.width / 2, canvas.height * 0.05);
-
-	//buttons
-	var minHeight = canvas.height * menu_settingMarginH;
-	var heightPerSetting = canvas.height * ((1 - (menu_settingMarginH * 2)) / menu_buttons_ai.length);
-
-	var minWidth = canvas.width * menu_settingMarginW;
-
-	ctx.font = `${canvas.height / 20}px Ubuntu`;
-	ctx.fillStyle = color_text;
-	ctx.textAlign = "left";
-
-	for (var e=0; e<menu_buttons_ai.length; e++) {
-		ctx.fillText(menu_buttons_ai[e][0], minWidth, minHeight + (heightPerSetting * e));
-	}
-
-	//button box
-	ctx.strokeStyle = color_text;
-	drawRoundedRectangle(minWidth - (canvas.width * 0.02), minHeight + (heightPerSetting * menu_selected) - (canvas.height / 30) - (canvas.height * 0.01), canvas.width * (0.5 - menu_settingMarginW * 2), canvas.height * 0.08666, canvas.height / 40);
-	ctx.stroke();
-
-	//AI listing, on the right
+	drawAIButtons();
+	drawAIListing();
 }
 
-function aiTrain_handleKeyPress(a) {
-	switch (a.key) {
-		case controls_s.esc:
-		case controls_s.rr:
-			game_state = 0;
-			game_substate = 0;
-			menu_selected = 0;
-			break;
-	}
-}
-
-function aiPlay_execute() {
+function execute_aiPlay() {
 	
 }
 
-function aiPlay_handleKeyPress(a) {
-	
-}
+
 
 
 //game mode functions
 
-function menu_execute() {
+function execute_menu() {
 	//background
 	audio_channel1.target = undefined;
 	audio_channel1.tick();
@@ -218,29 +190,7 @@ function menu_executePre() {
 
 function menu_executeNormal() {
 	//title text
-	ctx.font = `${canvas.height / 10}px Ubuntu`;
-	ctx.textAlign = "center";
-	ctx.fillStyle = color_text;
-
-	ctx.fillText(`Tetris`, canvas.width / 2, canvas.height * 0.08);
-
-	ctx.font = `${canvas.height / 20}px Ubuntu`;
-	//buttons
-	var spacePerButton = (menu_buttonHeightMax - menu_buttonHeightMin) / menu_buttons.length;
-	for (var b=0; b<menu_buttons.length; b++) {
-		ctx.fillText(menu_buttons[b][0], canvas.width / 2, canvas.height * (menu_buttonHeightMin + spacePerButton * b));
-	}
-
-	//instruction text
-	ctx.font = `${canvas.height / 40}px Ubuntu`;
-	ctx.fillText(`Use the arrow keys and Z to interact.`, canvas.width / 2, canvas.height * 0.95);
-
-	//selection box
-	var textWidth = ctx.measureText(menu_buttons[menu_selected][0]).width * 2;
-	var margin = canvas.height / 100;
-	ctx.strokeStyle = color_text;
-	drawRoundedRectangle((canvas.width / 2) - (textWidth / 2) - margin, canvas.height * (menu_buttonHeightMin + spacePerButton * (menu_selected - 0.45)), textWidth + margin * 2, canvas.height * spacePerButton * 0.9, canvas.height / 40);
-	ctx.stroke();
+	drawMainMenu();
 }
 
 function menu_executeSettings() {
@@ -280,39 +230,7 @@ function menu_executeSettings() {
 	ctx.stroke();
 }
 
-function menu_handleKeyPress(a) {
-	switch (a.key) {
-		case controls_s.u:
-			menu_selected = clamp(menu_selected - 1, 0, menu_selectSet.length-1);
-			while (menu_selectSet[menu_selected][0] == "") {
-				menu_selected = clamp(menu_selected - 1, 0, menu_selectSet.length-1);
-			}
-			break;
-		case controls_s.d:
-			menu_selected = clamp(menu_selected + 1, 0, menu_selectSet.length-1);
-			while (menu_selectSet[menu_selected][0] == "") {
-				menu_selected = clamp(menu_selected + 1, 0, menu_selectSet.length-1);
-			}
-			break;
-
-		//Z for select
-		case controls_s.rl:
-			if (menu_selectSet[menu_selected][1] != "") {
-				eval(menu_selectSet[menu_selected][1 + (game_substate > 0)]);
-			}
-			break;
-		//X for cancel
-		case controls_s.rr:
-			if (game_substate > 0) {
-				game_substate = 0;
-				menu_selectSet = menu_buttons;
-				menu_selected = 0;
-			}
-			break;
-	}
-}
-
-function endless_execute() {
+function execute_endless() {
 	//audio
 	audio_channel1.tick();
 
@@ -340,72 +258,17 @@ function endless_execute() {
 	}
 }
 
-function game_handleKeyPress(a) {
-	if (game_substate == 1) {
-		if (game_endOpacity == 1) {
-			game_endOpacity = 0;
-			game_substate = 0;
-			game_state = 0;
-		}
-		return;
-	}
-
-	switch (a.key) {
-		//left, right, and soft drop
-		case controls_s.l:
-			boards[0].movePiece(-1, 0);
-			break;
-		case controls_s.r:
-			boards[0].movePiece(1, 0);
-			break;
-		case controls_s.d:
-			boards[0].score += +boards[0].movePiece(0, 1);
-			break;
-
-		//rotation
-		case controls_s.rl:
-		case controls_s.rl2:
-			boards[0].twistPiece(1);
-			break;
-		case controls_s.rr:
-			boards[0].twistPiece(-1);
-			break;
-
-		//storage
-		case controls_s.st:
-			boards[0].storePiece();
-			break;
-
-		//hard drop
-		case controls_s.hd:
-			boards[0].hardDrop();
-			break;
-
-		//escape / enter
-		case controls_s.esc:
-			if (game_substate == 0) {
-				game_substate = 2;
-			} else {
-				game_state = 0;
-				game_substate = 0;
-			}
-			break;
-		case controls_s.confirm:
-			if (game_substate == 2) {
-				game_substate = 0;
-			}
-			break;
-	}
-}
-
-function sprint_execute() {
-	endless_execute();
+function execute_sprint() {
+	execute_endless();
 	//mark line number + time
-	ctx.fillStyle = color_text;
-	ctx.textAlign = "left";
-	ctx.fillText(`${Math.max(0, sprint_lines - boards[0].linesCleared)} Lines to go!`, canvas.width / 20, canvas.height * 0.95);
-	ctx.textAlign = "right";
-	ctx.fillText(`Time Taken: ${Math.round(boards[0].time / framesPerSecond)} s`, canvas.width * 19 / 20, canvas.height * 0.95);
+	if (game_substate == 0) {
+		ctx.fillStyle = color_text;
+		ctx.textAlign = "left";
+		ctx.fillText(`${Math.max(0, sprint_lines - boards[0].linesCleared)} Lines to go!`, canvas.width / 20, canvas.height * 0.95);
+		ctx.textAlign = "right";
+		ctx.fillText(`Time Taken: ${Math.round(boards[0].time / framesPerSecond)} s`, canvas.width * 19 / 20, canvas.height * 0.95);
+	}
+	
 	//if there are more than the necessary number of lines, pre-emptively end the game
 	if (boards[0].linesCleared > sprint_lines) {
 		boards[0].stopped = true;
@@ -413,7 +276,7 @@ function sprint_execute() {
 	}
 }
 
-function competition_execute() {
+function execute_competition() {
 	//background
 	ctx.fillStyle = color_palettes[0].bg;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -430,8 +293,4 @@ function competition_execute() {
 		drawGameOverText();
 		ctx.globalAlpha = 1;
 	}
-}
-
-function competition_handleKeyPress(a) {
-
 }
