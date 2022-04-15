a724617b0479f521ee661af9dd34725b6a2d6185
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2022-04-15 12:19:55 -0400

    Moved drawing functions here

--- tetris/functions-helper.js
+++ tetris/functions-helper.js
@@ -1,8 +1,41 @@
 
 
-//takes in score data in format [version, points, ]
+//takes in score data in format [name, points, version]
 function addHighScore(scoreData) {
-	
+	//first determine which score bin to place the score into
+	var version = scoreData.pop();
+
+	//add to the list
+	data_persistent.scores[version].push(scoreData);
+
+	//sort by points
+	data_persistent.scores[version] = data_persistent.scores[version].sort((a, b) => b[1] - a[1]);
+
+	//if there's too many scores, remove the worst one
+	while (data_persistent.scores[version].length > scores_max) {
+		data_persistent.scores[version].pop();
+	}
+}
+
+function clearLines(arrData) {
+	var len = arrData.length;
+	var numCleared = 0;
+
+	//if any line is filled all the way through, clear it
+	for (var l=0; l<arrData.length; l++) {
+		if (arrData[l].indexOf(null) == -1 && arrData[l].indexOf(undefined) == -1) {
+			arrData.splice(l, 1);
+			numCleared += 1;
+			l -= 1;
+		}
+	}
+
+	//fill any lines back up to the required length
+	while (arrData.length < len) {
+		arrData.splice(0, 0, new Array(board_width));
+	}
+
+	return numCleared;
 }
 
 //for making copies of objects that avoid reference issues
@@ -10,8 +43,49 @@ function copyObj(object) {
 	return JSON.parse(JSON.stringify(object));
 }
 
+function createBag() {
+	//final bag
+	var returnBag = [];
+	var pieceBag = ["I","J","L","O","S","T","Z"];
+
+	//shuffle
+	while (pieceBag.length > 0) {
+		returnBag.push(pieceBag.splice(Math.floor(Math.random() * pieceBag.length), 1)[0]);
+	}
+	return returnBag;
+}
 
 
+function drawAIButtons() {
+	//top text
+	ctx.font = `${canvas.height / 20}px Ubuntu`;
+	ctx.fillStyle = color_text;
+	ctx.textAlign = "center";
+	ctx.fillText(`AI Interface`, canvas.width / 2, canvas.height * 0.05);
+
+	//buttons
+	var minHeight = canvas.height * menu_settingMarginH;
+	var heightPerSetting = canvas.height * ((1 - (menu_settingMarginH * 2)) / menu_buttons_ai.length);
+
+	var minWidth = canvas.width * menu_settingMarginW;
+
+	ctx.font = `${canvas.height / 20}px Ubuntu`;
+	ctx.fillStyle = color_text;
+	ctx.textAlign = "left";
+
+	for (var e=0; e<menu_buttons_ai.length; e++) {
+		ctx.fillText(menu_buttons_ai[e][0], minWidth, minHeight + (heightPerSetting * e));
+	}
+
+	//button box
+	ctx.strokeStyle = color_text;
+	drawRoundedRectangle(minWidth - (canvas.width * 0.02), minHeight + (heightPerSetting * menu_selected) - (canvas.height / 30) - (canvas.height * 0.01), canvas.width * 0.35, canvas.height * 0.08666, canvas.height / 40);
+	ctx.stroke();
+}
+
+function drawAIListing() {
+
+}
 
 
 function drawGameOverScreen() {
@@ -69,6 +143,32 @@ function drawGameOverText(minX, maxX, minY, maxY, board) {
 	ctx.fillText(board.level,								maxX - (w / 10), minY + (h * 0.8));
 }
 
+function drawMainMenu() {
+	ctx.font = `${canvas.height / 10}px Ubuntu`;
+	ctx.textAlign = "center";
+	ctx.fillStyle = color_text;
+
+	ctx.fillText(`Tetris`, canvas.width / 2, canvas.height * 0.08);
+
+	ctx.font = `${canvas.height / 20}px Ubuntu`;
+	//buttons
+	var spacePerButton = (menu_buttonHeightMax - menu_buttonHeightMin) / menu_buttons.length;
+	for (var b=0; b<menu_buttons.length; b++) {
+		ctx.fillText(menu_buttons[b][0], canvas.width / 2, canvas.height * (menu_buttonHeightMin + spacePerButton * b));
+	}
+
+	//instruction text
+	ctx.font = `${canvas.height / 40}px Ubuntu`;
+	ctx.fillText(`Use the arrow keys, Z, and X to interact.`, canvas.width / 2, canvas.height * 0.95);
+
+	//selection box
+	var textWidth = ctx.measureText(menu_buttons[menu_selected][0]).width * 2;
+	var margin = canvas.height / 100;
+	ctx.strokeStyle = color_text;
+	drawRoundedRectangle((canvas.width / 2) - (textWidth / 2) - margin, canvas.height * (menu_buttonHeightMin + spacePerButton * (menu_selected - 0.45)), textWidth + margin * 2, canvas.height * spacePerButton * 0.9, canvas.height / 40);
+	ctx.stroke();
+}
+
 
 function drawRoundedRectangle(x, y, width, height, arcRadius) {
 	ctx.beginPath();
@@ -88,6 +188,47 @@ function enumerate(valueRef, enumRef, changeBy) {
 	var index = enumRef.indexOf(eval(valueRef));
 	var newIndex = (index + enumRef.length + changeBy) % enumRef.length;
 	eval(`${valueRef} = "${enumRef[newIndex]}"`);
+}
+
+function file_export() {
+	//first check to see if there's actually data to export
+	if (ai_populationPaired.length == 0) {
+		alert(`No data to export!`);
+		return;
+	}
+
+	//actually create the file
+	var textDat = JSON.stringify(ai_populationPaired);
+
+
+
+	//download said file
+	var newFile = new Blob([textDat], {type: 'text/plain'});
+
+	//make sure a file doesn't already exist (to avoid a memory leak)
+	if (fileObj != undefined) {
+		window.URL.revokeObjectURL(fileObj);
+	}
+	fileObj = window.URL.createObjectURL(newFile);
+	var link = document.getElementById('downloadBox');
+	link.href = fileObj;
+	link.click();
+
+
+}
+
+function file_import() {
+	//first get a reference to the file
+	var newFile = document.getElementById('uploadBox').files[0];
+	//read the file in, this is asynchronous
+	var fileReader = new FileReader();
+	fileReader.onload = function(fileLoadedEvent) {
+		//function for when the text actually loads
+		var textDat = fileLoadedEvent.target.result;
+		ai_populationPaired = JSON.parse(textDat);
+	};
+
+	fileReader.readAsText(newFile, "UTF-8");
 }
 
 function localStorage_read() {
